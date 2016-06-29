0055f9ee24c3bed4fabab57badbe56cd9a9b0a92
phlej001 <justin.x.phlegar.-nd@disney.com>
2016-06-29 14:28:20 -0400

    Updating BaseTest class to use methods smarter

--- src/test/java/com/disney/composite/BaseTest.java
+++ src/test/java/com/disney/composite/BaseTest.java
@@ -29,10 +29,20 @@ public class BaseTest {
 	}
 	
 	protected void validateLogs(BaseSoapService soap, LogItems logItems){
+		validate(true, soap, logItems);
+	}
+	
+	
+	protected void validateNotInLogs(BaseSoapService soap, LogItems logItems){
+		validate(false,soap,logItems);
+	}
+	
+	private void validate(boolean shouldBeInLogs, BaseSoapService soap, LogItems logItems){
 		boolean isValid = false;
 		boolean containsRequest = false;
 		boolean containsResponse = false;
 		if(!environment.equalsIgnoreCase("Grumpy")){
+				
 			Recordset rs = getLogs(environment, soap.getConversationID());
 			
 			for(LogItem item : logItems.getItems()){
@@ -58,13 +68,16 @@ public class BaseTest {
 											rs.getValue("BP_STEP").contains("Response")) ? true : false;
 					}
 					
-					//Ensure Response does not have an Error Code if an error is not expected
-					if(rs.getValue("LOG_LVL").equalsIgnoreCase("Exception")){
-						if((rs.getValue("SVC_CLASS").contains(item.getServiceClass()) || 
-								rs.getValue("SVC_OPERATION").equals(item.getServiceOperation()))){
-							if( item.isErrorExpected() == false){
-								System.out.println(rs.getValue("ERROR_CODE") + " : " +rs.getValue("LOG_MSG_TXT") );
-								throw new AutomationException(rs.getValue("ERROR_CODE") + " : " +rs.getValue("LOG_MSG_TXT"));							
+					if(shouldBeInLogs){
+						//Ensure Response does not have an Error Code if an error is not expected
+						if(rs.getValue("LOG_LVL").equalsIgnoreCase("Exception")){
+							if((rs.getValue("SVC_CLASS").contains(item.getServiceClass()) || 
+									rs.getValue("SVC_OPERATION").equals(item.getServiceOperation()))){
+								if( item.isErrorExpected() == false){
+									System.out.println(rs.getValue("ERROR_CODE") + " : " +rs.getValue("LOG_MSG_TXT") );
+									throw new AutomationException("Exception found in [" + item.getServiceClass() +"#"+ item.getServiceOperation() + "]\n" 
+									+ rs.getValue("ERROR_CODE") + " : " +rs.getValue("LOG_MSG_TXT"));							
+								}
 							}
 						}
 					}
@@ -78,58 +91,10 @@ public class BaseTest {
 				}
 	
 				// Throw exceptions if Request or Response is not found 
-				if(!containsRequest) throw new AutomationException("No request found for " + item.getServiceClass() + "#" + item.getServiceOperation());
-				if(!containsResponse) throw new AutomationException("No response found for " + item.getServiceClass() + "#" + item.getServiceOperation());
-			}
-		}else{
-			TestReporter.log("Skipping log validation for Grumpy");
-		}
-	
-	}
-	
-	
-	protected void validateNotInLogs(BaseSoapService soap, LogItems logItems){
-		boolean isValid = false;
-		boolean containsRequest = false;
-		boolean containsResponse = false;
-		if(!environment.equalsIgnoreCase("Grumpy")){
-				
-			Recordset rs = getLogs(environment, soap.getConversationID());
-			
-			for(LogItem item : logItems.getItems()){
-				isValid = false;
-				containsRequest = false;
-				containsResponse = false;
-				
-				System.out.println(item.getServiceClass() + "#" + item.getServiceOperation());
-				
-				for(rs.moveFirst() ; rs.hasNext() ; rs.moveNext()){
-					
-					//Ensures Request for desired operation exists
-					if(!containsRequest){
-						containsRequest = (rs.getValue("SVC_CLASS").contains(item.getServiceClass()) && 
-											rs.getValue("SVC_OPERATION").equals(item.getServiceOperation()) &&
-											rs.getValue("BP_STEP").contains("Request")) ? true : false;
-					}
-					
-					//Ensure Response for desired Operation exists
-					if(!containsResponse){
-						containsResponse = (rs.getValue("SVC_CLASS").contains(item.getServiceClass()) &&
-											rs.getValue("SVC_OPERATION").equals(item.getServiceOperation()) && 
-											rs.getValue("BP_STEP").contains("Response")) ? true : false;
-					}
-					
-					//Break out of loop once Request and Response is validated
-					if (containsRequest && containsResponse) {
-						isValid = true;
-						break;
-					}
-					
-				}
-	
-				// Throw exceptions if Request or Response is not found 
-				if(containsRequest) throw new AutomationException("Unexpected request found for [" + item.getServiceClass() + "#" + item.getServiceOperation() + "]. Log id is [" + rs.getValue("LOG_ID") + "]");
-				if(containsResponse) throw new AutomationException("Unexpected response found for [" + item.getServiceClass() + "#" + item.getServiceOperation() + "]. Log id is [" + rs.getValue("LOG_ID") + "]");
+				if(!shouldBeInLogs && containsRequest) throw new AutomationException("Unexpected request found for [" + item.getServiceClass() + "#" + item.getServiceOperation() + "]");
+				if(!shouldBeInLogs && containsResponse) throw new AutomationException("Unexpected response found for [" + item.getServiceClass() + "#" + item.getServiceOperation() + "]");
+				if(shouldBeInLogs && !containsRequest) throw new AutomationException("Expected request not found for [" + item.getServiceClass() + "#" + item.getServiceOperation() + "]");
+				if(shouldBeInLogs && !containsResponse) throw new AutomationException("Expected response not found for [" + item.getServiceClass() + "#" + item.getServiceOperation() + "]");
 			}
 		}else{
 			TestReporter.log("Skipping log validation for Grumpy");
