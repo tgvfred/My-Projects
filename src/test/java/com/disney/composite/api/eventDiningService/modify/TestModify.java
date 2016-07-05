117ab397d523fe56d1f6900ec986bd6a5b430e08
phlej001 <justin.x.phlegar.-nd@disney.com>
2016-07-05 15:07:54 -0400

    Reworking happy path test and adding negative tests for Modify

--- src/test/java/com/disney/composite/api/eventDiningService/modify/TestModify.java
+++ src/test/java/com/disney/composite/api/eventDiningService/modify/TestModify.java
@@ -3,29 +3,28 @@ package com.disney.composite.api.eventDiningService.modify;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.disney.api.soapServices.eventDiningService.operations.Modify;
+import com.disney.composite.BaseTest;
 import com.disney.utils.TestReporter;
-import com.disney.utils.dataFactory.guestFactory.Guest;
+import com.disney.utils.dataFactory.database.LogItems;
 import com.disney.utils.dataFactory.guestFactory.HouseHold;
 import com.disney.utils.dataFactory.staging.bookSEReservation.EventDiningReservation;
 import com.disney.utils.dataFactory.staging.bookSEReservation.ScheduledEventReservation;
 
-public class TestModify {
+public class TestModify extends BaseTest{
 	// Defining global variables
-	protected String environment;
-	protected String TPS_ID = null;
 	protected ScheduledEventReservation res = null;
-	protected HouseHold hh = null; 
 	
 	@BeforeMethod(alwaysRun = true)
 	@Parameters({ "environment" })
-	public void setup(
-		String environment){this.environment = environment;
-		hh = new HouseHold(1);
-		hh.sendToApi(this.environment);
-		res = new EventDiningReservation(environment, hh);
+	public void setup(@Optional String environment){
+		this.environment = environment;
+		HouseHold hh = new HouseHold(1);
+		res = new EventDiningReservation(this.environment, hh);
 		res.book(ScheduledEventReservation.NOCOMPONENTSNOADDONS);
 	}
 	
@@ -34,9 +33,24 @@ public class TestModify {
 
 	@Test(groups = {"api", "regression", "dining", "eventDiningService"})
 	public void testModify(){
-		hh.addGuest(new Guest());
-		hh.sendToApi(this.environment);
-		res.modify().modifyPartyMix(hh);
-		TestReporter.assertEquals(res.getNumberOfGuests(), hh.getAllGuests().size(), "The number of guests ["+res.getNumberOfGuests()+"] does not match the expected number of guests ["+hh.getAllGuests().size()+"].");
+		Modify modify = new Modify(this.environment, "NoComponentsNoAddOns");
+		modify.setReservationNumber(res.getConfirmationNumber());
+		modify.setTravelPlanId(res.getTravelPlanId());
+		modify.setParty(res.party());
+		modify.setFacilityId(res.getFacilityId());
+		modify.setServiceStartDate(res.getServiceStartDate());
+		modify.setServicePeriodId(res.getServicePeriodId());
+		modify.setProductId(res.getProductId());
+		modify.sendRequest();
+		TestReporter.logAPI(!modify.getResponseStatus().equals("SUCCESS"),"The Response status was not SUCCESS as expected", modify);
+		
+
+		LogItems logItems = new LogItems();
+		logItems.addItem("ChargeGroupIF", "modifyGuestContainerChargeGroup", false);
+		logItems.addItem("ChargeGroupIF", "modifyRootChargeGroup", false);
+		logItems.addItem("EventDiningServiceIF", "modify", false);
+		logItems.addItem("TravelPlanServiceCrossReferenceV3", "updateOrder", false);
+		logItems.addItem("TravelPlanServiceCrossReferenceV3SEI", "updateOrder", false);
+		validateLogs(modify, logItems);
 	}
 }
