92f4e99994d718714b44acc10a2fb7f76bd9805b
phlej001 <justin.x.phlegar.-nd@disney.com>
2016-07-06 11:54:46 -0400

    Adding tests

--- src/test/java/com/disney/composite/api/partyV3/searchGuestIDByName/TestSearchGuestIDByName.java
+++ src/test/java/com/disney/composite/api/partyV3/searchGuestIDByName/TestSearchGuestIDByName.java
@@ -1,10 +1,13 @@
 package com.disney.composite.api.partyV3.searchGuestIDByName;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.disney.api.soapServices.core.BaseSoapCommands;
+import com.disney.api.soapServices.core.exceptions.XPathNotFoundException;
 import com.disney.api.soapServices.partyV3.operations.SearchGuestIDByName;
 import com.disney.composite.BaseTest;
 import com.disney.utils.Regex;
@@ -13,15 +16,17 @@ import com.disney.utils.dataFactory.guestFactory.Guest;
 
 public class TestSearchGuestIDByName  extends BaseTest{
 	private Guest guest = new Guest();
-	@BeforeMethod(alwaysRun = true)
+	@BeforeTest(alwaysRun = true)
 	@Parameters({ "environment" })
 	public void setup(@Optional String environment){
-		this.environment = environment;
+		this.environment = "Development";
 		guest.sendToApi(this.environment, true);
+		guest.sendToApi(this.environment, true);
+		
 	}
 	
-	@Test
-	public void searchGuestIDByName(){
+//	@Test
+	public void searchGuestIDByFullName(){
 		SearchGuestIDByName search = new SearchGuestIDByName(this.environment);
 		search.setGuestFirstName(guest.getFirstName());
 		search.setGuestLastName(guest.getLastName());
@@ -31,4 +36,39 @@ public class TestSearchGuestIDByName  extends BaseTest{
 		TestReporter.assertTrue(Regex.match("[0-9]+", search.getPartyId()), "The Party ID ["+search.getPartyId()+"] is not numeric as expected.");
 	}
 	
+
+	//@Test
+	public void searchGuestIDByFirstName(){
+		SearchGuestIDByName search = new SearchGuestIDByName(this.environment);
+		search.setGuestFirstName(guest.getFirstName());
+		search.setGuestLastName(BaseSoapCommands.REMOVE_NODE.toString());
+		search.sendRequest();
+		
+		TestReporter.logAPI(!search.getResponseStatusCode().contains("200"), search.getFaultString(), search);
+		TestReporter.assertTrue(Regex.match("[0-9]+", search.getPartyId()), "The Party ID ["+search.getPartyId()+"] is not numeric as expected.");
+	}
+
+
+	//@Test
+	public void searchGuestIDByLastName(){
+		SearchGuestIDByName search = new SearchGuestIDByName(this.environment);
+		search.setGuestFirstName(BaseSoapCommands.REMOVE_NODE.toString());
+		search.setGuestLastName(guest.getLastName());
+		search.sendRequest();
+		
+		TestReporter.logAPI(!search.getResponseStatusCode().contains("200"), search.getFaultString(), search);
+		TestReporter.assertTrue(Regex.match("[0-9]+", search.getPartyId()), "The Party ID ["+search.getPartyId()+"] is not numeric as expected.");
+	}
+	
+
+	@Test(expectedExceptions=XPathNotFoundException.class)
+	public void searchGuestIDByInvalidNames(){
+		SearchGuestIDByName search = new SearchGuestIDByName(this.environment);
+		search.setGuestFirstName("MissingName");
+		search.setGuestLastName("InvalidName");
+		search.sendRequest();
+		
+		TestReporter.logAPI(!search.getResponseStatusCode().contains("200"), search.getFaultString(), search);
+		TestReporter.assertTrue(Regex.match("[0-9]+", search.getPartyId()), "The Party ID ["+search.getPartyId()+"] is not numeric as expected.");
+	}
 }
