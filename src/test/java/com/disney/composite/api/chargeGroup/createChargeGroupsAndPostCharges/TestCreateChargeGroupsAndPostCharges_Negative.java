bc44f674d2f09f1937e5f98341895199f1cbdd66
AVERW003 <waightstill.w.avery.-nd@disney.com>
2016-07-08 15:44:31 -0400

    developing tests for ChargeGroupServicePort#createChargeGroupsAndPostCharges

--- src/test/java/com/disney/composite/api/chargeGroup/createChargeGroupsAndPostCharges/TestCreateChargeGroupsAndPostCharges_Negative.java
+++ src/test/java/com/disney/composite/api/chargeGroup/createChargeGroupsAndPostCharges/TestCreateChargeGroupsAndPostCharges_Negative.java
@@ -11,6 +11,7 @@ import com.disney.api.soapServices.chargeGroup.operations.CreateChargeGroupsAndP
 import com.disney.api.soapServices.core.BaseSoapCommands;
 import com.disney.composite.BaseTest;
 import com.disney.utils.Randomness;
+import com.disney.utils.Regex;
 import com.disney.utils.TestReporter;
 import com.disney.utils.dataFactory.guestFactory.HouseHold;
 
@@ -46,7 +47,7 @@ public class TestCreateChargeGroupsAndPostCharges_Negative extends BaseTest{
 	}
 	
 	public class RootChargeGroup extends TestCreateChargeGroupsAndPostCharges_Negative{		
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
 		public void missingDescription(){
 			TestReporter.logScenario("Missing Description");
 			CreateChargeGroupsAndPostCharges create = create();
@@ -54,7 +55,7 @@ public class TestCreateChargeGroupsAndPostCharges_Negative extends BaseTest{
 			create.sendRequest();
 			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group - missing Description : Missing description"), create.getFaultString(), create);
 		}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
 		public void expiredPeriodEndDate(){
 			TestReporter.logScenario("Expired Period End Date");
 			CreateChargeGroupsAndPostCharges create = create();
@@ -62,7 +63,7 @@ public class TestCreateChargeGroupsAndPostCharges_Negative extends BaseTest{
 			create.sendRequest();
 			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group Period : ChargeGroup period provided is invalid."), create.getFaultString(), create);
 		}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
 		public void missingPeriod(){
 			TestReporter.logScenario("Missing Period");
 			CreateChargeGroupsAndPostCharges create = create();
@@ -70,7 +71,7 @@ public class TestCreateChargeGroupsAndPostCharges_Negative extends BaseTest{
 			create.sendRequest();
 			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group Period : Missing Period"), create.getFaultString(), create);
 		}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
 		public void missingPrimaryReferenceName(){
 			TestReporter.logScenario("Missing Primary Reference Name");
 			CreateChargeGroupsAndPostCharges create = create();
@@ -78,69 +79,156 @@ public class TestCreateChargeGroupsAndPostCharges_Negative extends BaseTest{
 			create.sendRequest();
 			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group - missing primary reference : ReferenceName and ReferenceValue both required for PrimaryReference.<null>,"+super.number), create.getFaultString(), create);
 		}
+//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+//		public void missingPrimaryReferenceValue(){
+//			TestReporter.logScenario("Missing Primary Reference Value");
+//			CreateChargeGroupsAndPostCharges create = create();
+//			create.setRootPrimaryReferenceValue(BaseSoapCommands.REMOVE_NODE.toString());
+//			create.sendRequest();
+//			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group - missing primary reference : ReferenceName and ReferenceValue both required for PrimaryReference.DREAMS_TPS,<null>"), create.getFaultString(), create);
+//		}
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingPrimaryReference(){
+			TestReporter.logScenario("Missing Primary Reference");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setRequestNodeValueByXPath("/Envelope/Body/createChargeGroupsAndPostCharges/rootChargeGroupRequest/primaryReference", BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group - missing primary reference : Missing primary reference"), create.getFaultString(), create);				
+		}
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingSourceAccountingCenter(){
+			TestReporter.logScenario("Missing Source Accounting Center");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setRootSourceAccountingCenter(BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			TestReporter.logAPI(!create.getFaultString().contains("Missing required fields. : SourceAccountingCenterID is required in ChargeGroupRequest. SourceAccountingCenterID=null"), create.getFaultString(), create);
+		}
+	}
+	
+	public class GuestContainerChargeGroup extends TestCreateChargeGroupsAndPostCharges_Negative{
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingDescription(){
+			TestReporter.logScenario("Missing Description");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setGuestContainerDecription(BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group - missing Description : Missing description"), create.getFaultString(), create);
+		}
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingPeriod(){
+			TestReporter.logScenario("Missing Period");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setRequestNodeValueByXPath("/Envelope/Body/createChargeGroupsAndPostCharges/guestContainerChargeGroupRequest/period", BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group Period : Missing Period"), create.getFaultString(), create);
+		}
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingPrimaryReferenceName(){
+			TestReporter.logScenario("Missing Primary Reference Name");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setGuestContainerPrimaryReferenceName(BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group - missing primary reference : ReferenceName and ReferenceValue both required for PrimaryReference.<null>,"+super.number), create.getFaultString(), create);
+		}
 		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
 		public void missingPrimaryReferenceValue(){
 			TestReporter.logScenario("Missing Primary Reference Value");
 			CreateChargeGroupsAndPostCharges create = create();
-			create.setRootPrimaryReferenceValue(BaseSoapCommands.REMOVE_NODE.toString());
+			create.setGuestContainerPrimaryReferenceValue(BaseSoapCommands.REMOVE_NODE.toString());
 			create.sendRequest();
-			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group - missing primary reference : ReferenceName and ReferenceValue both required for PrimaryReference."+super.number+",<null>"), create.getFaultString(), create);
+			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group - missing primary reference : ReferenceName and ReferenceValue both required for PrimaryReference.DREAMS_TPS,<null>"), create.getFaultString(), create);
 		}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingPrimaryReference(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingSourceAccountingCenter(){}
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingPrimaryReference(){
+			TestReporter.logScenario("Missing Primary Reference");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setRequestNodeValueByXPath("/Envelope/Body/createChargeGroupsAndPostCharges/guestContainerChargeGroupRequest/primaryReference", BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group - missing primary reference : Missing primary reference"), create.getFaultString(), create);			
+		}
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingSourceAccountingCenter(){
+			TestReporter.logScenario("Missing Source Accounting Center");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setGuestContainerSourceAccountingCenter(BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			TestReporter.logAPI(!create.getFaultString().contains("Missing required fields. : GuestChargeGroupContainerRequest is invalid. RootReference:DREAMS_TP,"+super.number+" SourceAccountingCenterID:null"), create.getFaultString(), create);
+		}
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingRootReference(){
+			TestReporter.logScenario("Missing Root Reference");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setRequestNodeValueByXPath("/Envelope/Body/createChargeGroupsAndPostCharges/guestContainerChargeGroupRequest/rootReference", BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			boolean match = Regex.match("Missing required fields. : GuestChargeGroupContainerRequest is invalid. RootReference:null SourceAccountingCenterID:[0-9]+ ResponsibleParty:[0-9]+", "Missing required fields. : GuestChargeGroupContainerRequest is invalid. RootReference:null SourceAccountingCenterID:3 ResponsibleParty:127313850");
+			TestReporter.logAPI(!match, create.getFaultString(), create);
+		}
 	}
 	
-	public class GuestContainerChargeGroup{
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingDescription(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void expiredPeriodEndDate(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingPeriod(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingPrimaryReferenceName(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingPrimaryReferenceValue(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingPrimaryReference(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingSourceAccountingCenter(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingRootReferenceName(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingRootReferenceValue(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingRootReference(){}
-	}
-	
-	public class GuestContainedChargeGroup{
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingDescription(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void expiredPeriodEndDate(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingPeriod(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingPrimaryReferenceName(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingPrimaryReferenceValue(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingPrimaryReference(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingSourceAccountingCenter(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingAncestorReferenceName(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingAncestorReferenceValue(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingAncestorReference(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingRootReferenceName(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingRootReferenceValue(){}
-//		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
-//		public void missingRootReference(){}
+	public class GuestContainedChargeGroup extends TestCreateChargeGroupsAndPostCharges_Negative{
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingDescription(){
+			TestReporter.logScenario("Missing Description");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setGuestContainedDecription(BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group - missing Description : Missing description"), create.getFaultString(), create);
+		}
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingPeriod(){
+			TestReporter.logScenario("Missing Period");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setRequestNodeValueByXPath("/Envelope/Body/createChargeGroupsAndPostCharges/guestContainedChargeGroupRequest/period", BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group Period : Missing Period"), create.getFaultString(), create);
+		}
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingPrimaryReferenceName(){
+			TestReporter.logScenario("Missing Primary Reference Name");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setGuestContainedPrimaryReferenceName(BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group - missing primary reference : ReferenceName and ReferenceValue both required for PrimaryReference.<null>,"+super.number), create.getFaultString(), create);
+		}
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingPrimaryReferenceValue(){
+			TestReporter.logScenario("Missing Primary Reference Value");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setGuestContainedPrimaryReferenceValue(BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group - missing primary reference : ReferenceName and ReferenceValue both required for PrimaryReference.DREAMS_TCG,<null>"), create.getFaultString(), create);
+		}
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingPrimaryReference(){
+			TestReporter.logScenario("Missing Primary Reference");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setRequestNodeValueByXPath("/Envelope/Body/createChargeGroupsAndPostCharges/guestContainedChargeGroupRequest/primaryReference", BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			TestReporter.logAPI(!create.getFaultString().contains("Invalid Charge Group - missing primary reference : Missing primary reference"), create.getFaultString(), create);
+		}
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingSourceAccountingCenter(){
+			TestReporter.logScenario("Missing Source Accounting Center");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setGuestContainedSourceAccountingCenter(BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			TestReporter.logAPI(!create.getFaultString().contains("Missing required fields. : SourceAccountingCenterID is required in ChargeGroupRequest. SourceAccountingCenterID=null"), create.getFaultString(), create);
+		}
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingAncestorReference(){
+			TestReporter.logScenario("Missing Ancestor");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setRequestNodeValueByXPath("/Envelope/Body/createChargeGroupsAndPostCharges/guestContainedChargeGroupRequest/ancestor", BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			TestReporter.logAPI(!create.getFaultString().contains("Missing required fields. : Invalid GuestChargeGroupContainedRequest. Ancestor, Root, GuaranteeType and TransactionFacility are required fields."), create.getFaultString(), create);
+		}
+		@Test(groups = {"api", "regression", "folio", "chargeGroupServicePort", "negative"})
+		public void missingRootReference(){
+			TestReporter.logScenario("Missing Ancestor");
+			CreateChargeGroupsAndPostCharges create = create();
+			create.setRequestNodeValueByXPath("/Envelope/Body/createChargeGroupsAndPostCharges/guestContainedChargeGroupRequest/root", BaseSoapCommands.REMOVE_NODE.toString());
+			create.sendRequest();
+			TestReporter.logAPI(!create.getFaultString().contains("Missing required fields. : Invalid GuestChargeGroupContainedRequest. Ancestor, Root, GuaranteeType and TransactionFacility are required fields."), create.getFaultString(), create);
+		}
 	}
 }
