8bddff0c7480666dedfaef02a9cf6e49a35d94f7
AVERW003 <waightstill.w.avery.-nd@disney.com>
2016-07-14 07:14:40 -0400

    developed service package and class for AccommodationInventoryRequestComponentServicePort, and the operation class for AccommodationInventoryRequestComponentServicePort#updateInventoryForScheduledEvents

---
+++ src/main/java/com/disney/api/soapServices/accommodationInventoryRequestComponentServicePort/AccommodationInventoryRequestComponentServicePort.java
@@ -0,0 +1,9 @@
+package com.disney.api.soapServices.accommodationInventoryRequestComponentServicePort;
+
+import com.disney.api.soapServices.core.BaseSoapService;
+
+public class AccommodationInventoryRequestComponentServicePort extends BaseSoapService {
+	public AccommodationInventoryRequestComponentServicePort(String environment) {	
+		setEnvironmentServiceURL("AccommodationInventoryRequestComponentServicePort", environment);	
+	}
+}
