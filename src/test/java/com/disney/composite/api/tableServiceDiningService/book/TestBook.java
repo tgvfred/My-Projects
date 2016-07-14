0fb96562f40edec50d6d4cbe06405b19a735e8a7
AVERW003 <waightstill.w.avery.-nd@disney.com>
2016-07-14 07:51:25 -0400

    verified success in parallel and refactored the test class to consolidate reusable and common code into private helper methods

--- src/test/java/com/disney/composite/api/tableServiceDiningService/book/TestBook.java
+++ src/test/java/com/disney/composite/api/tableServiceDiningService/book/TestBook.java
@@ -29,78 +29,48 @@ public class TestBook extends BaseTest{
 	
 	@Test(groups = {"api", "regression", "dining", "tableDiningService"})
 	public void testBook(){
-		hh = new HouseHold(1);
-		Book book = new Book(environment, "NoComponentsNoAddOns");
-		book.setParty(hh);
-		book.sendRequest();
-		TestReporter.logAPI(!book.getResponseStatusCode().contains("200"), book.getFaultString() ,book);
-		TestReporter.assertTrue(Regex.match("[0-9]+", book.getTravelPlanId()), "The travel plan ID ["+book.getTravelPlanId()+"] is not numeric as expected.");
-		TestReporter.assertTrue(Regex.match("[0-9]+", book.getTravelPlanSegmentId()), "The reservation number ["+book.getTravelPlanSegmentId()+"] is not numeric as expected.");
-		TPS_ID.set(book.getTravelPlanSegmentId());
-		
-		addAndValidateLogs(book);
+		TestReporter.logScenario("1 Adult");
+		hh = new HouseHold(1);	
+		addAndValidateLogs(bookAndValidate(hh));
 	}
 
 	@Test(groups = {"api", "regression", "dining", "tableDiningService"})
 	public void testBookWith2Adults(){
-		hh = new HouseHold("2 Adults");
-		Book book = new Book(environment, "NoComponentsNoAddOns");
-		book.setParty(hh);
-		book.sendRequest();
-		TestReporter.logAPI(!book.getResponseStatusCode().contains("200"), book.getFaultString() ,book);
-		TestReporter.assertTrue(Regex.match("[0-9]+", book.getTravelPlanId()), "The travel plan ID ["+book.getTravelPlanId()+"] is not numeric as expected.");
-		TestReporter.assertTrue(Regex.match("[0-9]+", book.getTravelPlanSegmentId()), "The reservation number ["+book.getTravelPlanSegmentId()+"] is not numeric as expected.");
-		TPS_ID.set(book.getTravelPlanSegmentId());
-		
-		addAndValidateLogs(book);
+		TestReporter.logScenario("2 Adults");
+		hh = new HouseHold("2 Adults");	
+		addAndValidateLogs(bookAndValidate(hh));
 	}
 
 	@Test(groups = {"api", "regression", "dining", "tableDiningService"})
 	public void testBookWith4Adults(){
-		hh = new HouseHold("4 Adults");
-		Book book = new Book(environment, "NoComponentsNoAddOns");
-		book.setParty(hh);
-		book.sendRequest();
-		TestReporter.logAPI(!book.getResponseStatusCode().contains("200"), book.getFaultString() ,book);
-		TestReporter.assertTrue(Regex.match("[0-9]+", book.getTravelPlanId()), "The travel plan ID ["+book.getTravelPlanId()+"] is not numeric as expected.");
-		TestReporter.assertTrue(Regex.match("[0-9]+", book.getTravelPlanSegmentId()), "The reservation number ["+book.getTravelPlanSegmentId()+"] is not numeric as expected.");
-		TPS_ID.set(book.getTravelPlanSegmentId());
-		
-		addAndValidateLogs(book);
+		TestReporter.logScenario("4 Adults");
+		hh = new HouseHold("4 Adults");	
+		addAndValidateLogs(bookAndValidate(hh));
 	}
 
 	@Test(groups = {"api", "regression", "dining", "tableDiningService"})
 	public void testBookWith2Adults2Child(){
-		hh = new HouseHold("2 Adults 2 Child");
-		Book book = new Book(environment, "NoComponentsNoAddOns");
-		book.setParty(hh);
-		book.sendRequest();
-		TestReporter.logAPI(!book.getResponseStatusCode().contains("200"), book.getFaultString() ,book);
-		TestReporter.assertTrue(Regex.match("[0-9]+", book.getTravelPlanId()), "The travel plan ID ["+book.getTravelPlanId()+"] is not numeric as expected.");
-		TestReporter.assertTrue(Regex.match("[0-9]+", book.getTravelPlanSegmentId()), "The reservation number ["+book.getTravelPlanSegmentId()+"] is not numeric as expected.");
-		TPS_ID.set(book.getTravelPlanSegmentId());
-		
-		addAndValidateLogs(book);
+		TestReporter.logScenario("2 Adults, 2 Children");
+		hh = new HouseHold("2 Adults 2 Child");	
+		addAndValidateLogs(bookAndValidate(hh));
 	}
 
 	@Test(groups = {"api", "regression", "dining", "tableDiningService"})
 	public void testBookWith4Adults2Child2Infant(){
-		hh = new HouseHold("4 Adults 2 Child 2 Infant");
-		Book book = new Book(environment, "NoComponentsNoAddOns");
-		book.setParty(hh);
-		book.sendRequest();
-		TestReporter.logAPI(!book.getResponseStatusCode().contains("200"), book.getFaultString() ,book);
-		TestReporter.assertTrue(Regex.match("[0-9]+", book.getTravelPlanId()), "The travel plan ID ["+book.getTravelPlanId()+"] is not numeric as expected.");
-		TestReporter.assertTrue(Regex.match("[0-9]+", book.getTravelPlanSegmentId()), "The reservation number ["+book.getTravelPlanSegmentId()+"] is not numeric as expected.");
-		TPS_ID.set(book.getTravelPlanSegmentId());
-		
-		addAndValidateLogs(book);
+		TestReporter.logScenario("4 Adults, 2 Children, 2 Infants");
+		hh = new HouseHold("4 Adults 2 Child 2 Infant");	
+		addAndValidateLogs(bookAndValidate(hh));
 	}
 	
 
 	@Test(groups = {"api", "regression", "dining", "tableDiningService"})
 	public void testBookWith12InParty(){
-		hh = new HouseHold(12);
+		TestReporter.logScenario("12 Adults");
+		hh = new HouseHold(12);		
+		addAndValidateLogs(bookAndValidate(hh));
+	}
+	
+	private Book bookAndValidate(HouseHold hh){
 		Book book = new Book(environment, "NoComponentsNoAddOns");
 		book.setParty(hh);
 		book.sendRequest();
@@ -108,8 +78,7 @@ public class TestBook extends BaseTest{
 		TestReporter.assertTrue(Regex.match("[0-9]+", book.getTravelPlanId()), "The travel plan ID ["+book.getTravelPlanId()+"] is not numeric as expected.");
 		TestReporter.assertTrue(Regex.match("[0-9]+", book.getTravelPlanSegmentId()), "The reservation number ["+book.getTravelPlanSegmentId()+"] is not numeric as expected.");
 		TPS_ID.set(book.getTravelPlanSegmentId());
-		
-		addAndValidateLogs(book);
+		return book;
 	}
 	
 	private void addAndValidateLogs(Book book){
