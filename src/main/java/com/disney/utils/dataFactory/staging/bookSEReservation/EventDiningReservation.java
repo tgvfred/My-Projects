52029f895ba855c15f9cdaf3f794fe319fe206fe
phlej001 <justin.x.phlegar.-nd@disney.com>
2016-07-19 19:32:54 -0400

    Fixing catch to skip Reservable Ressource for Dev

--- src/main/java/com/disney/utils/dataFactory/staging/bookSEReservation/EventDiningReservation.java
+++ src/main/java/com/disney/utils/dataFactory/staging/bookSEReservation/EventDiningReservation.java
@@ -243,7 +243,7 @@ public class EventDiningReservation implements ScheduledEventReservation {
 		eventDiningBook.setServiceStartDateTime(getServiceStartDate());
 		if(!agencyId.equals("0")){eventDiningBook.addTravelAgency(agencyId, agencyOdsId, guestTravelAgencyId, agentId, guestAgentId, confirmationLocatorValue, guestConfirmationLocationId);}	
 
-		if(getEnvironment().equalsIgnoreCase("Development")&& !getEnvironment().contains("_CM") ){
+		if(!getEnvironment().equalsIgnoreCase("Development")&& !getEnvironment().contains("_CM") ){
 			ReservableResourceByFacilityID resource = new ReservableResourceByFacilityID(getEnvironment(), "Main");
 			resource.setFacilityId(getFacilityId());
 			resource.sendRequest();
