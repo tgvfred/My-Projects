f56682cfc226afb8ec0fda4f447911b6a79d24f1
AVERW003 <waightstill.w.avery.-nd@disney.com>
2016-07-27 15:49:07 -0400

    developing tests for ScheduledEventsService

---
+++ src/test/java/com/disney/composite/api/scheduledEventsServicePort/massCancelSE/TestMassCancelSE_Negative.java
@@ -0,0 +1,19 @@
+package com.disney.composite.api.scheduledEventsServicePort.massCancelSE;
+
+import org.testng.annotations.Test;
+
+import com.disney.api.soapServices.applicationError.DiningErrorCode;
+import com.disney.api.soapServices.applicationError.LiloSystemErrorCode;
+import com.disney.api.soapServices.core.BaseSoapCommands;
+import com.disney.api.soapServices.scheduledEventsServicePort.operations.MassCancel;
+import com.disney.api.soapServices.scheduledEventsServicePort.operations.MassCancelSE;
+import com.disney.composite.BaseTest;
+import com.disney.utils.TestReporter;
+import com.disney.utils.dataFactory.database.LogItems;
+
+public class TestMassCancelSE_Negative extends BaseTest{
+	
+	
+
+
+}
