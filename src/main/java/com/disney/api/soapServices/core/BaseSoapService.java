f9792574e78beac1ccb37004521fb2280cbc469c
AVERW003 <waightstill.w.avery.-nd@disney.com>
2016-08-17 11:40:33 -0400

    debugging

--- src/main/java/com/disney/api/soapServices/core/BaseSoapService.java
+++ src/main/java/com/disney/api/soapServices/core/BaseSoapService.java
@@ -916,7 +916,7 @@ public abstract class BaseSoapService{
 			e.printStackTrace();
 		}
 
-		setServiceURL(getFirstNodeValueByTagName(responseDoc, "endPoint") + "?wsdl");
+		setServiceURL(getFirstNodeValueByTagName(responseDoc, "endPoint"));
 
 	}
 	
@@ -925,7 +925,7 @@ public abstract class BaseSoapService{
 	protected void setEnvironmentServiceURL(String service, String environment, String url) {
 		setEnvironment(environment);
 		setService(service);
-		setServiceURL(url + "?wsdl");
+		setServiceURL(url);
 	}
 	
 	//DJS - This was added as proof of concept for endpoints utilizing Service Virtualization
