95422c7843720eb172b0c62b86cadbab5bc94428
phlej001 <justin.x.phlegar.-nd@disney.com>
2016-08-18 08:07:17 -0400

    Adding logic and queries

--- src/main/java/com/disney/utils/dataFactory/database/sqlStorage/AvailSE.java
+++ src/main/java/com/disney/utils/dataFactory/database/sqlStorage/AvailSE.java
@@ -16,18 +16,30 @@ public class AvailSE {
 				 " where a.rsrvbl_rsrc_id = b.rsrvbl_rsrc_id " +
 				 " and to_Char(a.fsell_invtry_srvc_dts, 'yyyy-mm-dd') >= '" +startDate.substring(0, startDate.indexOf("T")) + "' " +
 				 " and b.RSRVBL_RSRC_ID = '" + resourceId + "' " +
+				 " and (AUTH_INVTRY_CN > 0 AND (BK_CN + FREEZE_CN) < AUTH_INVTRY_CN) " +
 				 " and rownum = 1 " + 
 				 " order by fsell_invtry_srvc_dts";
 	}
 	
 	public static String getReservableResourceByFacilityAndDate(String facilityId, String date){
-		return "select Resource_ID from "
-				+ "( select a.RSRVBL_RSRC_ID Resource_ID "
+		String trimDate = date.contains("T") ? date.substring(0, date.indexOf("T")) : date;
+		return "select Resource_ID, Start_Date from "
+				+ "( select a.RSRVBL_RSRC_ID Resource_ID, FSELL_INVTRY_SRVC_DTS Start_Date "
 				+ "from AVAILSE.fsell_invtry a, AVAILSE.rsrvbl_rsrc b "
 				+ "where a.rsrvbl_rsrc_id = b.rsrvbl_rsrc_id "
 				+ "and b.ENTRPRS_FAC_ID = '"+facilityId+"' "
-				+ "and to_char(a.FSELL_INVTRY_SRVC_DTS, 'yyyy-mm-dd') = '"+date+"' "
+				+ "and to_char(a.FSELL_INVTRY_SRVC_DTS, 'yyyy-mm-dd') = '"+trimDate+"' "
+				+ "and (AUTH_INVTRY_CN > 0 AND (BK_CN + FREEZE_CN) < AUTH_INVTRY_CN) "
 				+ "order by dbms_random.value ) "
 				+ "where rownum = 1";
+	}
+	
+	public static String getFreezeId(String resourceId, String date){
+		return "select RSRVBL_RSRC_ID, FSELL_INVTRY_SRVC_DTS, FREEZE_ID "
+			 + "from AVAILSE.fsell_invtry a, "
+		     + "AVAILSE.FREEZE_FSELL_INVTRY b "
+		     + "WHERE a.FSELL_INVTRY_ID = b.FSELL_INVTRY_ID "
+		     + "AND a.RSRVBL_RSRC_ID =  '" + resourceId + "' " 
+		     + "and to_char(a.FSELL_INVTRY_SRVC_DTS, 'yyyy-mm-dd') = '"+date+"'  ";
 	}
 }
