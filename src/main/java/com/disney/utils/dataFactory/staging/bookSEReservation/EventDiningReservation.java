c96a3e59484eac97d139a83e9a139e3a3cd56051
phlej001 <justin.x.phlegar.-nd@disney.com>
2016-08-19 11:32:14 -0400

    Finished EventDining fixes

--- src/main/java/com/disney/utils/dataFactory/staging/bookSEReservation/EventDiningReservation.java
+++ src/main/java/com/disney/utils/dataFactory/staging/bookSEReservation/EventDiningReservation.java
@@ -269,13 +269,13 @@ public class EventDiningReservation implements ScheduledEventReservation{
 		eventDiningBook.setServiceStartDateTime(getServiceStartDate());
 		if(!agencyId.equals("0")){eventDiningBook.addTravelAgency(agencyId, agencyOdsId, guestTravelAgencyId, agentId, guestAgentId, confirmationLocatorValue, guestConfirmationLocationId);}	
 
-		if(!getEnvironment().equalsIgnoreCase("Development")&& !getEnvironment().contains("_CM") ){
+		/*if(!getEnvironment().equalsIgnoreCase("Development")&& !getEnvironment().contains("_CM") ){
 			ReservableResourceByFacilityID resource = new ReservableResourceByFacilityID(getEnvironment(), "Main");
 			resource.setFacilityId(getFacilityId());
 			resource.sendRequest();
 			resource.getReservableResources();
 			eventDiningBook.setReservableResourceId(resource.getFirstReservableResourceId());
-		}
+		}*/
 		
 		Sleeper.sleep(Randomness.randomNumberBetween(1, 10) * 1000);
 		eventDiningBook.sendRequest();
