b2b9bc187e31723c250e5c95cb2a0ff12c2430db
AVERW003 <waightstill.w.avery.-nd@disney.com>
2016-08-19 07:28:08 -0400

    developing tests for compensation flows

---
+++ src/test/java/com/disney/composite/api/activityModule/activityService/_compensationFlow/book/TestCompensationFlow_Book_Negative.java
@@ -0,0 +1,21 @@
+package com.disney.composite.api.activityModule.activityService._compensationFlow.book;
+
+import org.testng.annotations.Test;
+
+import com.disney.composite.BaseTest;
+
+public class TestCompensationFlow_Book_Negative extends BaseTest{
+	
+	@Test(groups = {"api", "regression", "activity", "activityService"})
+	public void testRimFailure(){
+		
+	}
+	@Test(groups = {"api", "regression", "activity", "activityService"})
+	public void testDineFailure(){
+		
+	}
+	@Test(groups = {"api", "regression", "activity", "activityService"})
+	public void testFolioFailure(){
+		
+	}
+}
---
+++ src/test/java/com/disney/composite/api/activityModule/activityService/_compensationFlow/book/TestCompensationFlow_Book_Positive.java
@@ -0,0 +1,13 @@
+package com.disney.composite.api.activityModule.activityService._compensationFlow.book;
+
+import org.testng.annotations.Test;
+
+import com.disney.composite.BaseTest;
+
+public class TestCompensationFlow_Book_Positive extends BaseTest{
+	
+	@Test(groups = {"api", "regression", "activity", "activityService"})
+	public void testCompensationFlow_Positive(){
+		
+	}
+}
