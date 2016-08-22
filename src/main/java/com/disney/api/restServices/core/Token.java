99f8443383707eaea5fd4eb42d1684ca64a34439
Keith McCarthy <keith.j.mccarthy.-nd@disney.com>
2016-08-22 12:31:57 -0400

    Retry to push classes that were not committed earlier

---
+++ src/main/java/com/disney/api/restServices/core/Token.java
@@ -0,0 +1,15 @@
+package com.disney.api.restServices.core;
+
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+
+public class Token {
+	public void demo(){
+		/**List<NameValuePair> params = new ArrayList()<NameValuePair>();
+		    params.add(new BasicNameValuePair("authenticity_token", this.authenticity_token));
+		    params.add(new BasicNameValuePair("employee[username]", username));
+		restService.sendPostRequest(resource, HeaderType.BASIC_CONVO, params, "");
+	**/}
+
+}
---
+++ src/main/java/com/disney/api/restServices/folio/chargeAccountServiceV2/chargeAccount/retrieveGuests/retrieveGuests.java
@@ -0,0 +1,30 @@
+package com.disney.api.restServices.folio.chargeAccountServiceV2.chargeAccount.retrieveGuests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.Header;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicHeader;
+
+import com.disney.api.restServices.core.Headers.HeaderType;
+import com.disney.api.restServices.core.RestResponse;
+import com.disney.api.restServices.core.RestService;
+import com.disney.test.utils.Randomness;
+
+public class retrieveGuests {
+	private RestService restService;
+	private String resource = "/retrieveGuests";
+	public retrieveGuests(RestService restService, String resource){
+		
+		this.restService = restService;
+		this.resource = resource + this.resource;
+	}
+	
+	public RestResponse sendPutRequest(String json){	
+		
+		return restService.sendPutRequest(resource, HeaderType.REST, json);		
+	}
+	
+	
+}
