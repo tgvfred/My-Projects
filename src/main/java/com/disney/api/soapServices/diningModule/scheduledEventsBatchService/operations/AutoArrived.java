cd3ff2eee778ff3140a8ad37d2bed4fb4d55b7c6
AVERW003 <waightstill.w.avery.-nd@disney.com>
2016-08-23 09:41:32 -0400

    developing tests for compensation flow

--- src/main/java/com/disney/api/soapServices/diningModule/scheduledEventsBatchService/operations/AutoArrived.java
+++ src/main/java/com/disney/api/soapServices/diningModule/scheduledEventsBatchService/operations/AutoArrived.java
@@ -2,8 +2,14 @@ package com.disney.api.soapServices.diningModule.scheduledEventsBatchService.ope
 
 import com.disney.api.soapServices.diningModule.scheduledEventsBatchService.ScheduledEventsBatchService;
 import com.disney.utils.XMLTools;
+import com.disney.utils.dataFactory.database.Database;
+import com.disney.utils.dataFactory.database.Recordset;
+import com.disney.utils.dataFactory.database.databaseImpl.OracleDatabase;
+import com.disney.utils.dataFactory.database.sqlStorage.AvailSE;
 
 public class AutoArrived extends ScheduledEventsBatchService{
+	protected String inventoryBefore;
+	protected String inventoryAfter;
 	public AutoArrived(String environment, String scenario) {
 		super(environment);
 
@@ -16,4 +22,18 @@ public class AutoArrived extends ScheduledEventsBatchService{
 	}
 
 	public void setTpsId(String value){setRequestNodeValueByXPath("/Envelope/Body/autoArrived/tpsIds", value);}
+	
+	public void sendRequest(String rrId, String dateTime){
+		inventoryBefore = getInventory(rrId, dateTime);
+		super.sendRequest();
+		inventoryAfter = getInventory(rrId, dateTime);		
+	}
+	private String getInventory(String rrId, String dateTime){
+		Database db = new OracleDatabase(getEnvironment(), Database.AVAIL_SE);
+		Recordset rsInventory = new Recordset(db.getResultSet(AvailSE.getAvailableResourceCount(rrId, dateTime)));
+		rsInventory.print();
+		return rsInventory.getValue("BK_CN");
+	}
+	public String getInventoryCountBefore(){return inventoryBefore;}
+	public String getInventoryCountAfter(){return inventoryAfter;}
 }
