486c67c49b476e209ee91c8884226edb69e7f590
AVERW003 <waightstill.w.avery.-nd@disney.com>
2016-08-26 14:22:51 -0400

    developing tests for compensation flow

--- src/test/java/com/disney/composite/api/diningModule/eventDiningService/_compensationFlow/book/TestCompensationFlow_Book_Negative.java
+++ src/test/java/com/disney/composite/api/diningModule/eventDiningService/_compensationFlow/book/TestCompensationFlow_Book_Negative.java
@@ -4,6 +4,8 @@ import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
+import com.disney.api.soapServices.applicationError.FolioErrorCode;
+import com.disney.api.soapServices.core.BaseSoapCommands;
 import com.disney.api.soapServices.diningModule.eventDiningService.operations.Book;
 import com.disney.api.soapServices.diningModule.eventDiningService.operations.Cancel;
 import com.disney.composite.BaseTest;
@@ -11,20 +13,26 @@ import com.disney.utils.Randomness;
 import com.disney.utils.TestReporter;
 
 public class TestCompensationFlow_Book_Negative extends BaseTest{
-	private Book book;
+	private ThreadLocal<Book> book = new ThreadLocal<Book>();
 	
 	@AfterMethod(alwaysRun=true)
 	public void teardown(){
 		try{
 			Cancel cancel = new Cancel(environment, "CancelDiningEvent");
-			cancel.setReservationNumber(book.getTravelPlanSegmentId());
+			cancel.setReservationNumber(book.get().getTravelPlanSegmentId());
 			cancel.sendRequest();
 		}catch(Exception e){}
 	}
 	
 	@Test(groups = {"api", "regression", "activity", "activityService", "negative", "compensation"})
 	public void TestCompensationFlow_Book_Negative_RIMFail(){
-		throw new SkipException("The testing solution for this scenario has not been determined.");
+		TestReporter.logScenario("Test Positive Activity Book Compensation Flow");
+		book.set(new Book(environment, "NoComponentsNoAddOns"));
+		book.get().setParty(hh);
+		book.get().setFreezeIdForError(Randomness.randomAlphaNumeric(36));
+		book.get().sendRequest();
+		TestReporter.logAPI(!book.get().getResponse().contains("RELEASE INVENTORY REQUEST IS INVALID"), book.get().getFaultString(), book.get());
+		TestReporter.assertTrue(Integer.parseInt(book.get().getInventoryCountBefore()) == Integer.parseInt(book.get().getInventoryCountAfter()), "Verify the booked inventory count ["+book.get().getInventoryCountAfter()+"] for reservable resource ID ["+book.get().getReservableResourceId()+"] increments from the count prior to booking ["+book.get().getInventoryCountBefore()+"]");
 	}
 	@Test(groups = {"api", "regression", "activity", "activityService", "negative", "compensation"})
 	public void TestCompensationFlow_Book_Negative_DineFail(){
@@ -32,6 +40,15 @@ public class TestCompensationFlow_Book_Negative extends BaseTest{
 	}
 	@Test(groups = {"api", "regression", "activity", "activityService", "negative", "compensation"})
 	public void TestCompensationFlow_Book_Negative_FolioFail(){
-		throw new SkipException("The testing solution for this scenario has not been determined.");
+		TestReporter.logScenario("Test Positive Activity Book Compensation Flow");
+		book.set(new Book(environment, "NoComponentsNoAddOns"));
+		book.get().setParty(hh);
+		book.get().setFacilityId("90002032");
+		book.get().addDetailsByProductName("Hoop-Dee-Doo-Cat 2-1st Show");
+		book.get().setRequestNodeValueByXPath("/Envelope/Body/book/bookEventDiningRequest/eventDiningPackage/componentPrices[1]/unitPrices/taxes/revenueType", BaseSoapCommands.REMOVE_NODE.toString());
+		book.get().sendRequest();
+		TestReporter.logAPI(!book.get().getFaultString().contains("Invalid input fields"), book.get().getFaultString() ,book.get());
+		validateApplicationError(book.get(), FolioErrorCode.INVALID_INPUT);
+		TestReporter.assertTrue(Integer.parseInt(book.get().getInventoryCountBefore()) == Integer.parseInt(book.get().getInventoryCountAfter()), "Verify the booked inventory count ["+book.get().getInventoryCountAfter()+"] for reservable resource ID ["+book.get().getReservableResourceId()+"] increments from the count prior to booking ["+book.get().getInventoryCountBefore()+"]");
 	}
 }
