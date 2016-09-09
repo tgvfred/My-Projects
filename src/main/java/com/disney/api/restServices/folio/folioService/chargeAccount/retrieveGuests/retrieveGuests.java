a17d43f917a24e007035ee1c45d49fe90099d115
phlej001 <justin.x.phlegar.-nd@disney.com>
2016-09-09 10:50:52 -0400

    Adding param options to sendGetRequest

--- src/main/java/com/disney/api/restServices/folio/folioService/chargeAccount/retrieveGuests/retrieveGuests.java
+++ src/main/java/com/disney/api/restServices/folio/folioService/chargeAccount/retrieveGuests/retrieveGuests.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.http.Header;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
 
 import com.disney.api.restServices.core.Headers.HeaderType;
 import com.disney.api.restServices.core.RestResponse;
@@ -15,7 +16,7 @@ import com.disney.test.utils.Randomness;
 
 public class retrieveGuests {
 	private RestService restService;
-	private String resource = "/retrieveGuests";
+	private String resource = "/retrieveguests";
 	public retrieveGuests(RestService restService, String resource){
 		
 		this.restService = restService;
@@ -33,4 +34,11 @@ public class retrieveGuests {
 		return restService.sendPutRequest(resource, HeaderType.REST_NOAuth, json);
 	}
 	
+	public RestResponse sendGetRequest(String referenceName, String referenceValue, String sourceAccountCenter){
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+	    params.add(new BasicNameValuePair("referenceName", referenceName));
+	    params.add(new BasicNameValuePair("referenceValue", referenceValue));
+	    params.add(new BasicNameValuePair("sourceAccountingCenter", sourceAccountCenter));
+		return restService.sendGetRequest(resource, HeaderType.REST, params);
+	}
 }
