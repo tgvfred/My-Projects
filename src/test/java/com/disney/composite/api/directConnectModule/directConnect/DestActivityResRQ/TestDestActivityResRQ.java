6feb39cfd496ccd8afc50af010ebd7e5a065c863
Keith McCarthy <keith.j.mccarthy.-nd@disney.com>
2016-09-10 11:58:00 -0400

    Updates for DC DigitalTicketing

--- src/test/java/com/disney/composite/api/directConnectModule/directConnect/DestActivityResRQ/TestDestActivityResRQ.java
+++ src/test/java/com/disney/composite/api/directConnectModule/directConnect/DestActivityResRQ/TestDestActivityResRQ.java
@@ -1,29 +1,58 @@
 package com.disney.composite.api.directConnectModule.directConnect.DestActivityResRQ;
 
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import com.disney.api.soapServices.directConnect.operations.DestActivityResRQ;
 import com.disney.composite.BaseTest;
 import com.disney.test.utils.Sleeper;
 import com.disney.utils.dataFactory.database.Database;
 import com.disney.utils.Randomness;
+import com.disney.utils.Regex;
+import com.disney.utils.TestReporter;
 import com.disney.utils.dataFactory.database.Recordset;
 import com.disney.utils.dataFactory.database.databaseImpl.OracleDatabase;
 
 public class TestDestActivityResRQ extends BaseTest {
 	// Defining global variables
+	private String environment ="Bashful";
+	private String uniqueIdentifier = Randomness.randomAlphaNumeric(15);
+	private String uniqueID = Randomness.randomNumber(7);
+	private String RequestID;
 	
-	//@Test(groups = {"api", "regression", "directConnect", "OTA_CancelRQ"})
-		@Test
-		public void testCancel(){
-			DestActivityResRQ res = new DestActivityResRQ(environment, "NoComponentsNoAddOns");
-			
-			res.sendRequest();
-			if(res.getResponse().toLowerCase().contains("unique constraint")){
-				Sleeper.sleep(Randomness.randomNumberBetween(1, 5) * 1000);
-				res.sendRequest();
-			}
-			Database db = new OracleDatabase("Bashful", Database.DIRECT_CONNECT);
-	        Recordset rs = new Recordset(db.getResultSet("select T.RESPONSE_MSG from ecmdc.transactions t where t.conversation_id = '201608221705KJM' and t.trans_status_cd in ('Failed','Success')"));
-	        rs.print();
-		}
+	@BeforeClass(alwaysRun = true)
+	@Parameters({ "environment" })
+	public void  setup(@Optional String environment) {
+		this.environment = "Bashful"; //temp solution
+		
+	}
+
+	
+	@Test(groups={ "regression", "smoke", "dirctconnect", "DestActivityResRQ", "api"})
+	public void TestDestActivityResRQ_SuccessfulDTIBooking(){
+		TestReporter.setDebugLevel(TestReporter.DEBUG);
+		DestActivityResRQ destactivityresrq = new DestActivityResRQ(environment , "Positive");
+		destactivityresrq.setPayloadRequestId(uniqueIdentifier);
+		destactivityresrq.setEchoToken(uniqueIdentifier);
+		destactivityresrq.setUniqueId(uniqueID);
+		destactivityresrq.sendRequest();
+		TestReporter.logAPI(!destactivityresrq.getResponseStatusCode().contains("200"), destactivityresrq.getFaultString() ,destactivityresrq);
+		RequestID = destactivityresrq.getRequestId();
+		TestReporter.assertTrue(Regex.match("[0-9A-Za-z]+",destactivityresrq.getRequestId()) , "The uniqueIdentifier is ["+RequestID+"]");
+		//DB call
+		Database db = new OracleDatabase("Bashful", Database.DIRECT_CONNECT);
+		Recordset rs = new Recordset(db.getResultSet("select T.RESPONSE_MSG from ecmdc.transactions t where t.conversation_id = '"+uniqueIdentifier+"' and t.trans_status_cd in ('Failed','Success')"));
+		//Write the results to the report
+		rs.print(); 
+		//Convert the Recordset to a string
+		String responseMessage = rs.printString();
+		//Grab the section where the error messages are located
+		Object testvalue = responseMessage.substring(4564, 4582);
+		//Convert to a string
+		String reservationNum = testvalue.toString();
+		//Print out the error code and validate
+		System.out.println(reservationNum);
+		TestReporter.assertTrue(reservationNum.contains("Error Code=\"DC7505"),"The message contains the proper error code of: Error Code=\"DC7505\" ShortText=\"Product Not Permitted\" Type=\"unknown\">Product Not Permitted</Error ");
+	}
 }
--- src/test/java/com/disney/composite/api/directConnectModule/directConnect/DestActivityResRQ/TestDestActivityResRQ_Negative.java
+++ src/test/java/com/disney/composite/api/directConnectModule/directConnect/DestActivityResRQ/TestDestActivityResRQ_Negative.java
@@ -25,8 +25,7 @@ public class TestDestActivityResRQ_Negative {
 			
 		}
 
-		
-		@Test(groups={ "regression", "smoke", "dirctconnect", "DestActivityResRQ", "api"})
+		@Test(groups={ "regression", "smoke", "dirctconnect","digitalticketing","negative", "DestActivityResRQ", "api"})
 		public void TestDestActivityResRQ_Negative_InvalidProduct(){
 			TestReporter.setDebugLevel(TestReporter.DEBUG);
 			DestActivityResRQ destactivityresrq = new DestActivityResRQ(environment , "Main");
@@ -37,14 +36,71 @@ public class TestDestActivityResRQ_Negative {
 			TestReporter.logAPI(!destactivityresrq.getResponseStatusCode().contains("200"), destactivityresrq.getFaultString() ,destactivityresrq);
 			RequestID = destactivityresrq.getRequestId();
 			TestReporter.assertTrue(Regex.match("[0-9A-Za-z]+",destactivityresrq.getRequestId()) , "The uniqueIdentifier is ["+RequestID+"]");
-			//DB call
+			//DB call to grab DC message
 			Database db = new OracleDatabase("Bashful", Database.DIRECT_CONNECT);
 			Recordset rs = new Recordset(db.getResultSet("select T.RESPONSE_MSG from ecmdc.transactions t where t.conversation_id = '"+uniqueIdentifier+"' and t.trans_status_cd in ('Failed','Success')"));
+			//Write the results to the report
 			rs.print(); 
-			
-		
+			//Convert the Recordset to a string
+			String responseMessage = rs.printString();
+			//Grab the section where the error messages are located
+			Object testvalue = responseMessage.substring(4564, 4582);
+			//Convert to a string
+			String errorCode = testvalue.toString();
+			//Print out the error code and validate
+			System.out.println(errorCode);
+			TestReporter.assertTrue(errorCode.contains("Error Code=\"DC7505"),"The message contains the proper error code of: Error Code=\"DC7505\" ShortText=\"Product Not Permitted\" Type=\"unknown\">Product Not Permitted</Error ");
 		}
-		
-		
-
+		@Test(groups={ "regression", "smoke", "dirctconnect","digitalticketing","negative", "DestActivityResRQ", "api"})
+		public void TestDestActivityResRQ_Negative_41Tickets(){
+			TestReporter.setDebugLevel(TestReporter.DEBUG);
+			DestActivityResRQ destactivityresrq = new DestActivityResRQ(environment , "MaxTickets");
+			destactivityresrq.setPayloadRequestId(uniqueIdentifier);
+			destactivityresrq.setEchoToken(uniqueIdentifier);
+			destactivityresrq.setUniqueId(uniqueID);
+			destactivityresrq.sendRequest();
+			TestReporter.logAPI(!destactivityresrq.getResponseStatusCode().contains("200"), destactivityresrq.getFaultString() ,destactivityresrq);
+			RequestID = destactivityresrq.getRequestId();
+			TestReporter.assertTrue(Regex.match("[0-9A-Za-z]+",destactivityresrq.getRequestId()) , "The uniqueIdentifier is ["+RequestID+"]");
+			//DB call to grab DC message
+			Database db = new OracleDatabase("Bashful", Database.DIRECT_CONNECT);
+			Recordset rs = new Recordset(db.getResultSet("select T.RESPONSE_MSG from ecmdc.transactions t where t.conversation_id = '"+uniqueIdentifier+"' and t.trans_status_cd in ('Failed','Success')"));
+			//Write the results to the report
+			rs.print(); 
+			//Convert the Recordset to a string
+			String responseMessage = rs.printString();
+			//Grab the section where the error messages are located
+			Object testvalue = responseMessage.substring(4486, 4504);
+			//Convert to a string
+			String errorCode = testvalue.toString();
+			//Print out the error code and validate
+			System.out.println(errorCode);
+			TestReporter.assertTrue(errorCode.contains("Error Code=\"DC0462"),"The message contains the proper error code of: Error Code=\"DC0462\"ShortText=\"Ticket quantity exceeded.\" Type=\"unknown\">Ticket quantity exceeded.");
+		}
+		@Test(groups={ "regression", "smoke", "dirctconnect","digitalticketing","negative", "DestActivityResRQ", "api"})
+		public void TestDestActivityResRQ_Negative_CannotConnectToSystemOfRecord(){
+			TestReporter.setDebugLevel(TestReporter.DEBUG);
+			DestActivityResRQ destactivityresrq = new DestActivityResRQ(environment , "Main");
+			destactivityresrq.setPayloadRequestId(uniqueIdentifier);
+			destactivityresrq.setEchoToken(uniqueIdentifier);
+			destactivityresrq.setUniqueId(uniqueID);
+			destactivityresrq.sendRequest();
+			TestReporter.logAPI(!destactivityresrq.getResponseStatusCode().contains("200"), destactivityresrq.getFaultString() ,destactivityresrq);
+			RequestID = destactivityresrq.getRequestId();
+			TestReporter.assertTrue(Regex.match("[0-9A-Za-z]+",destactivityresrq.getRequestId()) , "The uniqueIdentifier is ["+RequestID+"]");
+			//DB call to grab DC message
+			Database db = new OracleDatabase("Bashful", Database.DIRECT_CONNECT);
+			Recordset rs = new Recordset(db.getResultSet("select T.RESPONSE_MSG from ecmdc.transactions t where t.conversation_id = '"+uniqueIdentifier+"' and t.trans_status_cd in ('Failed','Success')"));
+			//Write the results to the report
+			rs.print(); 
+			//Convert the Recordset to a string
+			String responseMessage = rs.printString();
+			//Grab the section where the error messages are located
+			Object testvalue = responseMessage.substring(4564, 4582);
+			//Convert to a string
+			String errorCode = testvalue.toString();
+			//Print out the error code and validate
+			System.out.println(errorCode);
+			TestReporter.assertTrue(errorCode.contains("Error Code=\"DC0022"),"The message contains the proper error code of: Error Code=\"DC0022\"ShortText=\"Could not connect to System of Record.\" Type=\"unknown\">Could not connect to System of Record.");
+		}
 }
