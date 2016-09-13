2f6c21a68282b9f629bd7b21b066fae46ee854bd
phlej001 <justin.x.phlegar.-nd@disney.com>
2016-09-13 12:15:52 -0400

    Adding Light/Dark capabilities

--- src/main/java/com/disney/api/restServices/core/Headers.java
+++ src/main/java/com/disney/api/restServices/core/Headers.java
@@ -50,66 +50,84 @@ public class Headers {
 	   */
 	  public static Header[]  createHeader(HeaderType type)  {
 		  Header[] headers = null;
-	        switch(type) {
-		        case JENKINS:		        	
-        			TestReporter.logInfo("Creating headers for [JENKINS]");
-		        	headers = new Header[] {
-		    	   		    new BasicHeader("Content-type", "application/x-www-form-urlencoded")};
-		        	break;
-		        case AUTH:		        	
-        			TestReporter.logInfo("Creating headers for [AUTH]");
+		  String zddHeader = System.getenv("header");
+		  if(zddHeader == null) zddHeader = System.getProperty("header");
+		  if(zddHeader != null) {				
+			  zddHeader = zddHeader.equalsIgnoreCase("Dark") ? "SHADOW" : zddHeader;
+			  zddHeader = zddHeader.equalsIgnoreCase("Shadow") ? "SHADOW" : "";
 
-		        	headers= new Header[] {
-		    	   		     new BasicHeader("Content-type", "application/x-www-form-urlencoded")};
-		        	break;
-		        case BASIC_CONVO:
-        			TestReporter.logInfo("Creating headers for [BASIC_CONVO]");
-		        	headers = new Header[] {
-		    	   		     new BasicHeader("Content-type", "application/json;charset=utf-8")
-		    	   		    ,new BasicHeader("Accept", "application/json")
-		    	   		    ,new BasicHeader("username", "test116.user")
-		    	   		    ,new BasicHeader("messageId", Randomness.generateMessageId())
-		    	   		    ,new BasicHeader("Connection", "keep-alive")
-		    	   		    ,new BasicHeader("conversationId",  Randomness.generateConversationId())
-		    	   		    ,new BasicHeader("requestedTimestamp", Randomness.generateCurrentXMLDatetime() + ".000-04:00")
-		    	   		};
-		        	break;
-		        case REST:
-		        	TestReporter.logInfo("Creating headers for [REST]");
-		        	List<NameValuePair> params = new ArrayList<NameValuePair>();
-		        	params.add(new BasicNameValuePair("grant_type", "password"));
-		        	params.add(new BasicNameValuePair("username", "mdxcontc@ngetestmail.com"));
-		        	params.add(new BasicNameValuePair("password", "mickey1"));
-		        	params.add(new BasicNameValuePair("client_id", "SE_TEST_EXTERNAL_PASSWORD"));
-		        	params.add(new BasicNameValuePair("assertion_type", "disneyid"));
-					AuthToken authToken = null;
-					try {
-						authToken = new RestService().sendPostRequest(new URI("https://stg.authorization.go.com/token"), HeaderType.AUTH, params).mapJSONToObject(AuthToken.class);
-					} catch (URISyntaxException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+			  if(zddHeader.equals("SHADOW")) TestReporter.logInfo("Request will be sent to Dark Side");
+			  else TestReporter.logInfo("Request will be sent to Light Side");
+		  }
+		  else {
+			  TestReporter.logInfo("Request will be sent to Light Side");
+			  zddHeader = "";
+		  }
+		  
+		  switch(type) {
+		  case JENKINS:		        	
+			  TestReporter.logInfo("Creating headers for [JENKINS]");
+			  headers = new Header[] {
+					  new BasicHeader("Content-type", "application/x-www-form-urlencoded"),
+					  new BasicHeader("X-Disney-Internal-PoolOverride",zddHeader)
+					  };
+			  break;
+		  case AUTH:		        	
+			  TestReporter.logInfo("Creating headers for [AUTH]");
 
-		        	headers = new Header []{
-		        			new BasicHeader("authorization", authToken.getTokenType() + " " + authToken.getAccessToken())
-		        	};
-		        	break;
-		        case REST_NOAuth:
-		        	headers = new Header []{
-		        			//No Authorization Token will be sent
-		        	};
-		        break;
-	            default:
-	                break;
-	        }
-	        
-	        //Logging headers
-	        String allHeaders = "";
-	    	for (Header header : headers){
-	    		allHeaders += "[" +header.getName() + ": " + header.getValue()+"] ";
-	    	}
-			TestReporter.logInfo("Headers added " + allHeaders);
-			
-			return headers;
+			  headers= new Header[] {
+					  new BasicHeader("Content-type", "application/x-www-form-urlencoded")};
+			  break;
+		  case BASIC_CONVO:
+			  TestReporter.logInfo("Creating headers for [BASIC_CONVO]");
+			  headers = new Header[] {
+					  new BasicHeader("Content-type", "application/json;charset=utf-8")
+					  ,new BasicHeader("Accept", "application/json")
+					  ,new BasicHeader("username", "test116.user")
+					  ,new BasicHeader("messageId", Randomness.generateMessageId())
+					  ,new BasicHeader("Connection", "keep-alive")
+					  ,new BasicHeader("conversationId",  Randomness.generateConversationId())
+					  ,new BasicHeader("requestedTimestamp", Randomness.generateCurrentXMLDatetime() + ".000-04:00"),
+					  new BasicHeader("X-Disney-Internal-PoolOverride",zddHeader)
+			  };
+			  break;
+		  case REST:
+			  TestReporter.logInfo("Creating headers for [REST]");
+			  List<NameValuePair> params = new ArrayList<NameValuePair>();
+			  params.add(new BasicNameValuePair("grant_type", "password"));
+			  params.add(new BasicNameValuePair("username", "mdxcontc@ngetestmail.com"));
+			  params.add(new BasicNameValuePair("password", "mickey1"));
+			  params.add(new BasicNameValuePair("client_id", "SE_TEST_EXTERNAL_PASSWORD"));
+			  params.add(new BasicNameValuePair("assertion_type", "disneyid"));
+			  AuthToken authToken = null;
+			  try {
+				  authToken = new RestService().sendPostRequest(new URI("https://stg.authorization.go.com/token"), HeaderType.AUTH, params).mapJSONToObject(AuthToken.class);
+			  } catch (URISyntaxException e) {
+				  // TODO Auto-generated catch block
+				  e.printStackTrace();
+			  }
+
+			  headers = new Header []{
+					  new BasicHeader("authorization", authToken.getTokenType() + " " + authToken.getAccessToken()),
+					  new BasicHeader("X-Disney-Internal-PoolOverride",zddHeader)
+			  };
+			  break;
+		  case REST_NOAuth:
+			  headers = new Header []{
+					  //No Authorization Token will be sent
+			  };
+			  break;
+		  default:
+			  break;
+		  }
+
+		  //Logging headers
+		  String allHeaders = "";
+		  for (Header header : headers){
+			  allHeaders += "[" +header.getName() + ": " + header.getValue()+"] ";
+		  }
+		  TestReporter.logInfo("Headers added " + allHeaders);
+
+		  return headers;
 	    }
 }
