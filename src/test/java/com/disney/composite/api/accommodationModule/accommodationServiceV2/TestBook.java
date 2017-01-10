e98a3c9aeb538675fcc263c96159f067490d1a92
phlej001 <justin.x.phlegar.-nd@disney.com>
2017-01-10 13:04:30 -0500

    Updates

--- src/test/java/com/disney/composite/api/accommodationModule/accommodationServiceV2/TestBook.java
+++ src/test/java/com/disney/composite/api/accommodationModule/accommodationServiceV2/TestBook.java
@@ -3,17 +3,13 @@ package com.disney.composite.api.accommodationModule.accommodationServiceV2;
 import org.testng.annotations.Test;
 
 import com.disney.api.soapServices.accommodationModule.accommodationServiceV2.operations.Book;
+import com.disney.utils.Randomness;
 import com.disney.utils.TestReporter;
 
 public class TestBook {
 
 	@Test
 	public void sandbox(){
-		Book book = new Book("Grumpy", "MinimalInfo");
-		System.out.println(book.getRequest());
-		book.sendRequest();
-		
-		System.out.println(book.getResponse());
-		TestReporter.logAPI(false, "blah", book);
+		System.out.println(Randomness.generateCurrentXMLDate(-30));
 	}
 }
