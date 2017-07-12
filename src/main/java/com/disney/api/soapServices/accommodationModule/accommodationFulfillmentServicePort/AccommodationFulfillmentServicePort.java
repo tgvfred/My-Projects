f914a6708166ddb5554a95ed7e3e8e34b2a541c1
phlej001 <justin.x.phlegar.-nd@disney.com>
2017-07-11 22:49:27 -0400

    Adding xml references

--- src/main/java/com/disney/api/soapServices/accommodationModule/accommodationFulfillmentServicePort/AccommodationFulfillmentServicePort.java
+++ src/main/java/com/disney/api/soapServices/accommodationModule/accommodationFulfillmentServicePort/AccommodationFulfillmentServicePort.java
@@ -4,12 +4,13 @@ import com.disney.api.soapServices.core.BaseSoapService;
 
 
 public class AccommodationFulfillmentServicePort extends BaseSoapService {
-	public AccommodationFulfillmentServicePort() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public AccommodationFulfillmentServicePort(String environment) {	
-		setEnvironmentServiceURL("AccommodationFulfillmentServicePort", environment);	
-	}
+    public AccommodationFulfillmentServicePort() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public AccommodationFulfillmentServicePort(String environment) {
+        setEnvironmentServiceURL("AccommodationFulfillmentServicePort", environment);
+        setXmlRepo("xml/accommodationFulfillmentServicePort");
+    }
 
 }
