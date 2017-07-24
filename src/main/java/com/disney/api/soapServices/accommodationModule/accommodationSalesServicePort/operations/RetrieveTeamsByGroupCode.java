5156a20fdd92d7843fad2fa8ca83a051e3397f8e
brian dowtin <Brian.K.Dowtin.-ND@disney.com>
2017-07-24 16:02:33 -0400

    2nd Pass before review

--- src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesServicePort/operations/RetrieveTeamsByGroupCode.java
+++ src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesServicePort/operations/RetrieveTeamsByGroupCode.java
@@ -1,5 +1,12 @@
 package com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.operations;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.soap.Node;
+
+import org.w3c.dom.NodeList;
+
 import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.AccommodationSalesServicePort;
 import com.disney.utils.XMLTools;
 
@@ -21,5 +28,16 @@ public class RetrieveTeamsByGroupCode extends AccommodationSalesServicePort{
 	}
 	
 	public String getTeamCode() {return getResponseNodeValueByXPath("/Envelope/Body/retrieveTeamsByGroupCodeResponse/response/teamNames");}
-	
+	public int getCountOfTeamCode() {return getNumberOfResponseNodesByXPath("/Envelope/Body/retrieveTeamsByGroupCodeResponse/response/teamNames");}
+	public List<String> getTeamNames(){
+		List<String> teamNames = new ArrayList<>();
+		NodeList nodes = XMLTools.getNodeList(getResponseDocument(), "/Envelope/Body/retrieveTeamsByGroupCodeResponse/response/teamNames");
+		for(int x = 0 ; x < nodes.getLength() ; x++ ){
+			teamNames.add(nodes.item(x).getFirstChild().getTextContent());
+		}
+		
+		return teamNames;
+	}
+	//public String[] getTeamCodes() {return getResponseNodeValueByXPath("/Envelope/Body/retrieveTeamsByGroupCodeResponse/response/teamNames");}
 }
+													
--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/TestRetrieveTeamByGroupCode.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/TestRetrieveTeamByGroupCode.java
@@ -1,5 +1,7 @@
 package com.disney.composite.api.accommodationModule.soapServices.accommodationSalesServicePort;
 
+import java.util.List;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -19,25 +21,30 @@ private String environment = "";
 	@Parameters({  "environment" })
 	public void setup(String environment) {
 		this.environment = environment;
-	
 	}
 
 @Test(groups={"api", "regression", "accommodation", "accommodationSalesService", "RetrieveTeamsByGroupCode"})
 public void testRetrieveTeamsByGroupCodeMissingGroupCode(){
 	
 	RetrieveTeamsByGroupCode RetrieveTeamsByGroupCode = new RetrieveTeamsByGroupCode(environment, "" );
-	RetrieveTeamsByGroupCode.setgroupcode("");
+	//RetrieveTeamsByGroupCode.setgroupcode("");
 	RetrieveTeamsByGroupCode.sendRequest();
 	System.out.println(RetrieveTeamsByGroupCode.getRequest());
 	System.out.println(RetrieveTeamsByGroupCode.getResponse());
 	TestReporter.assertEquals(RetrieveTeamsByGroupCode.getResponseStatusCode(), "200", "The response code was not 200");
+	try {
+		TestReporter.assertNull( RetrieveTeamsByGroupCode.getTeamCode(), "The team code retrieved: "+  RetrieveTeamsByGroupCode.getTeamCode() + ".   Expected null" );
+	} catch (Exception e) {
+		// continue
+	}finally {
+		// clean ups
+	}
+
 }
 
-	
 
 @Test(groups={"api", "regression", "accommodation", "accommodationSalesService", "RetrieveTeamsByGroupCode"})
-public void testRetrieveTeamsByGroupCodeMainFlow(){
-
+public void testRetrieveTeamsByGroupCodeSingle(){
 
 	String sql = "select a.grp_cd, a.grp_tm_nm " +
 			"from res_mgmt.grp_tm a " +
@@ -54,50 +61,132 @@ public void testRetrieveTeamsByGroupCodeMainFlow(){
 	Recordset rs = new Recordset(db.getResultSet(sql));
 	rs.print();
 
-	//rs.getvalue(row, column);
-
-	//TestReporter.assertTrue(true, "Assert Failed - Here is the assert failure");
-
-   String groupCode = rs.getValue(1,1);
-   String teamCode = rs.getValue(2,1);
+	String groupCode = rs.getValue(1,1);
+	String teamCode = rs.getValue(2,1);
 
 	RetrieveTeamsByGroupCode RetrieveTeamsByGroupCode = new RetrieveTeamsByGroupCode(environment, "" );
-		RetrieveTeamsByGroupCode.setgroupcode(groupCode);
-		RetrieveTeamsByGroupCode.sendRequest();
+	RetrieveTeamsByGroupCode.setgroupcode(groupCode);
+	RetrieveTeamsByGroupCode.sendRequest();
 
+	//Debug
+	//System.out.println(RetrieveTeamsByGroupCode.getRequest());
+	//System.out.println(RetrieveTeamsByGroupCode.getResponse());
+
+	String teamCodeRetrieved = RetrieveTeamsByGroupCode.getTeamCode();
+
+	//Debug
+	System.out.println("teamcode: " + teamCode);
+	System.out.println("teamCodeRetr: " + teamCodeRetrieved);
+
+	TestReporter.assertEquals(RetrieveTeamsByGroupCode.getResponseStatusCode(), "200", "The response code was not 200");
+
+	TestReporter.assertEquals(teamCode,  teamCodeRetrieved, "The team code retrieved: "+ teamCode + " does not match teamCode in db: " + teamCodeRetrieved );
+}	
+
+
+@Test(groups={"api", "regression", "accommodation", "accommodationSalesService", "RetrieveTeamsByGroupCode"})
+public void testRetrieveTeamsByGroupCodeMulitple(){
+
+	String sql = "select a.grp_cd, a.grp_tm_nm " +
+			"from res_mgmt.grp_tm a " +
+			"where a.grp_cd in ( " +
+			"select * " +
+			"from (select a.grp_cd " +
+			"from res_mgmt.grp_tm a " +
+			"group by a.grp_cd " +
+			"having count(a.grp_tm_nm) >= 2 " +      
+			"order by dbms_random.value )  " +
+			"where rownum < 2 ) ";
+
+	Database db = new OracleDatabase(environment, Database.DREAMS);
+	Recordset rs = new Recordset(db.getResultSet(sql));
+	rs.print();
+
+
+   String groupCode = rs.getValue(1,1);
+   String teamCode;
+
+	RetrieveTeamsByGroupCode retrieveTeamsByGroupCode = new RetrieveTeamsByGroupCode(environment, "" );
+	retrieveTeamsByGroupCode.setgroupcode(groupCode);
+	retrieveTeamsByGroupCode.sendRequest();
 		
-		System.out.println(RetrieveTeamsByGroupCode.getRequest());
-		System.out.println(RetrieveTeamsByGroupCode.getResponse());
+		TestReporter.logAPI(!retrieveTeamsByGroupCode.getResponseStatusCode().equals("200"), "The response code was not 200", retrieveTeamsByGroupCode);
 		
-		String teamCodeRetrieved = RetrieveTeamsByGroupCode.getTeamCode();
-		
+		System.out.println(retrieveTeamsByGroupCode.getCountOfTeamCode());
+		List<String> teams = retrieveTeamsByGroupCode.getTeamNames();
+	
+		System.out.println();
 		//Debug
-		System.out.println("teamcode: " + teamCode);
-		System.out.println("teamCodeRetr: " + teamCodeRetrieved);
+		//System.out.println(RetrieveTeamsByGroupCode.getRequest());
+		//System.out.println(RetrieveTeamsByGroupCode.getResponse());
 		
-		TestReporter.assertEquals(RetrieveTeamsByGroupCode.getResponseStatusCode(), "200", "The response code was not 200");
+		String teamCodeRetrieved = retrieveTeamsByGroupCode.getTeamCode();
 		
-		TestReporter.assertEquals(teamCode,  teamCodeRetrieved, "The team code retrieved: "+ teamCode + " does not match teamCode in db: " + teamCodeRetrieved );
+		// Compare Each Row
+		int rows = rs.getRowCount();
+		boolean found = false;
+		for (rs.moveFirst(); rs.hasNext(); rs.moveNext()) {
+			for(String name : teams){
+				if(name.equalsIgnoreCase(rs.getValue("grp_tm_nm"))){
+					found = true;
+					break;
+				}	
+			}
+			if(found){
+				TestReporter.softAssertTrue(true, "Team name [ " + rs.getValue("grp_tm_nm") + " ] was found as expected");
+			}else{
+				TestReporter.softAssertTrue(false, "Team name [ " + rs.getValue("grp_tm_nm") + " ] was not found as expected");
+			}
+			found = false;
+		}
+		
+		TestReporter.assertAll();
+	}
+
+
+@Test(groups={"api", "regression", "accommodation", "accommodationSalesService", "RetrieveTeamsByGroupCode"})
+public void testRetrieveTeamsByGroupCodeNoTeamNames(){
+
+	String sql = "select a.grp_cd, b.grp_tm_nm " +
+		"from group_mgmt.grp a " +
+		"left outer join res_mgmt.grp_tm b on a.grp_cd = b.grp_cd " +
+		"where b.grp_tm_nm is null " +
+		"and rownum <= 5 " +
+		"order by dbms_random.value" ;
+
+	Database db = new OracleDatabase(environment, Database.DREAMS);
+	Recordset rs = new Recordset(db.getResultSet(sql));
+	rs.print();
+
+
+	String groupCode = rs.getValue(1,1);
+	String teamCode;
+
+	RetrieveTeamsByGroupCode retrieveTeamsByGroupCode = new RetrieveTeamsByGroupCode(environment, "" );
+	retrieveTeamsByGroupCode.setgroupcode(groupCode);
+	retrieveTeamsByGroupCode.sendRequest();
+
+	TestReporter.logAPI(!retrieveTeamsByGroupCode.getResponseStatusCode().equals("200"), "The response code was not 200", retrieveTeamsByGroupCode);
+
+	System.out.println(retrieveTeamsByGroupCode.getCountOfTeamCode());
+	//List<String> teams = retrieveTeamsByGroupCode.getTeamNames();
+	try	{
+	String teamCodeRetrieved = retrieveTeamsByGroupCode.getTeamCode();
+	
+	TestReporter.softAssertNull(teamCodeRetrieved, "Team Names unexpectedly found. Should be nil ");
+
+	}
+	catch (Exception e) {
+		// no catch
+	} finally {
+		TestReporter.assertAll();	
+		System.out.println("End Finally on No Team Names");
+	}	
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
+
+
+
+
+
