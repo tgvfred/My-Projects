4b452c164602cfde581ee666c3f3d3e0faa5249e
DETSB001 <Billy.F.Detschel.-ND@disney.com>
2017-07-26 15:05:19 -0400

    More Test Cases

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesComponentServicePort/getAccommodationExternalReferences/Test_GetAccommodationExternalReferences_bookingWithNoAccommExtRef.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesComponentServicePort/getAccommodationExternalReferences/Test_GetAccommodationExternalReferences_bookingWithNoAccommExtRef.java
@@ -1,19 +1,38 @@
 package com.disney.composite.api.accommodationModule.soapServices.accommodationSalesComponentServicePort.getAccommodationExternalReferences;
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.disney.api.soapServices.accommodationModule.accommodationSalesComponentServicePort.operations.GetAccommodationExternalReferences;
 import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
+import com.disney.api.soapServices.core.BaseSoapCommands;
 import com.disney.utils.Environment;
 import com.disney.utils.TestReporter;
-import com.disney.utils.dataFactory.database.Database;
-import com.disney.utils.dataFactory.database.Recordset;
-import com.disney.utils.dataFactory.database.databaseImpl.OracleDatabase;
 
 public class Test_GetAccommodationExternalReferences_bookingWithNoAccommExtRef extends AccommodationBaseTest {
 
     private String extNum;
     private String extSrc;
+
+    @Override
+    @BeforeMethod(alwaysRun = true)
+    @Parameters("environment")
+    public void setup(String environment) {
+        setEnvironment(environment);
+        setDaysOut(0);
+        setNights(1);
+        setArrivalDate(getDaysOut());
+        setDepartureDate(getDaysOut() + getNights());
+        setValues(environment);
+        setSendRequest(false);
+        bookReservation();
+
+        getBook().setRequestNodeValueByXPath("/Envelope/Body/replaceAllForTravelPlanSegment/request/roomDetails/externalReferences", BaseSoapCommands.REMOVE_NODE.toString());
+        getBook().sendRequest();
+        TestReporter.logAPI(!getBook().getResponseStatusCode().equals("200"), "Verify that no error occurred booking a reservation: " + getBook().getFaultString(), getBook());
+
+    }
 
     @Test(groups = { "api", "regression", "accommodation", "accommodationComponentSalesService", "GetAccommodationExternalReferences" })
     public void testGetAccommodationExternalReferences_bookingWithNoAccommExtRef() {
@@ -24,8 +43,8 @@ public class Test_GetAccommodationExternalReferences_bookingWithNoAccommExtRef e
         get.sendRequest();
         TestReporter.logAPI(!get.getResponseStatusCode().equals("200"), "An error occurred retrieving the summary for the travel component grouping [" + getBook().getTravelComponentGroupingId() + "]", get);
 
-        // Compares the response values to the DB values
-        validation(get.getExternalReferenceNumber(), get.getExternalReferenceSource());
+        // Validate that no external references are returned
+        TestReporter.assertTrue(get.getResponseNodeValueByXPath("/Envelope/Body/getAccommodationExternalReferencesResponse").isEmpty(), "No External References Found!");
 
         // Old vs New Validation
         if (Environment.isSpecialEnvironment(environment)) {
@@ -35,29 +54,9 @@ public class Test_GetAccommodationExternalReferences_bookingWithNoAccommExtRef e
             if (!clone.getResponseStatusCode().equals("200")) {
                 TestReporter.logAPI(!clone.getResponseStatusCode().equals("200"), "Error was returned", clone);
             }
-            clone.addExcludedBaselineAttributeValidations("@xsi:nil");
-            clone.addExcludedBaselineAttributeValidations("@xsi:type");
-            clone.addExcludedBaselineXpathValidations("/Envelope/Header");
             TestReporter.assertTrue(clone.validateResponseNodeQuantity(get, true), "Validating Response Comparison");
         }
 
-    }
-
-    public void validation(String num, String src) {
-
-        String sql = "select * from res_mgmt.TC_EXTNL_REF a where a.TC_EXTNL_REF_VL = '" + num + "'";
-
-        Database db = new OracleDatabase(Environment.getBaseEnvironmentName(environment), Database.DREAMS);
-        Recordset rs = new Recordset(db.getResultSet(sql));
-
-        extNum = rs.getValue("TC_EXTNL_REF_VL");
-        extSrc = rs.getValue("TC_EXTNL_SRC_NM");
-
-        TestReporter.assertEquals(extNum, num, "Verify the External Reference Number [" + num + "] matches the External Reference Number found"
-                + " in the DB [" + extNum + "]");
-
-        TestReporter.assertEquals(extSrc, src, "Verify the External Reference Source [" + src + "] matches the External Reference Source found"
-                + " in the DB [" + extSrc + "]");
     }
 
 }
