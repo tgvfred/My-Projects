c3e4429e1eae12c41a253bd7fc9b940305382ea3
ROWAF001 <Francis.J.Rowan.-ND@disney.com>
2017-07-27 15:06:49 -0400

    Structural Buildout

---
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationBatchComponentWSPort/getStagedRecordsForRemoveGroup/TestGetStagedRecordsForRemoveGroup_Positive.java
@@ -0,0 +1,7 @@
+package com.disney.composite.api.accommodationModule.soapServices.accommodationBatchComponentWSPort.getStagedRecordsForRemoveGroup;
+
+import com.disney.api.BaseTest;
+
+public class TestGetStagedRecordsForRemoveGroup_Positive extends BaseTest {
+
+}
