515412314088e7e5f214899af040ed84062f1b03
averw003 <waightstill.w.avery.-nd@disney.com>
2017-07-28 08:24:07 -0400

    line endings

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesComponentServicePort/checkout/Checkout_roomOnly_DVC.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesComponentServicePort/checkout/Checkout_roomOnly_DVC.java
@@ -1,4 +1,3 @@
-
 package com.disney.composite.api.accommodationModule.soapServices.accommodationSalesComponentServicePort.checkout;
 
 import org.testng.annotations.BeforeMethod;
@@ -6,40 +5,43 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.disney.api.soapServices.accommodationModule.accommodationSalesComponentServicePort.operations.Checkout;
-import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
 import com.disney.api.soapServices.accommodationModule.helpers.CheckInHelper;
+import com.disney.api.soapServices.accommodationModule.helpers.ValidationHelper;
 import com.disney.api.soapServices.core.BaseSoapCommands;
+import com.disney.api.soapServices.dvcModule.dvcSalesService.helpers.BookDVCCashHelper;
 import com.disney.utils.Randomness;
+import com.disney.utils.Sleeper;
 import com.disney.utils.TestReporter;
 import com.disney.utils.dataFactory.database.Database;
 import com.disney.utils.dataFactory.database.Recordset;
 import com.disney.utils.dataFactory.database.databaseImpl.OracleDatabase;
 
-public class checkout_WDTC extends AccommodationBaseTest {
+public class Checkout_roomOnly_DVC extends BookDVCCashHelper{
 	private CheckInHelper helper;
-
-	@Override
-	@Parameters("environment")
-	@BeforeMethod(alwaysRun = true)
-	public void setup(String environment) {
-		setEnvironment(environment);
-		setDaysOut(0);
-		setNights(1);
-		setArrivalDate(getDaysOut());
-		setDepartureDate(getDaysOut() + getNights());
-		setValues(getEnvironment());
-		setIsWdtcBooking(true);
-		setAddNewGuest(true);
-		bookReservation();
-	}
-	public String validateResMgmt(String TcId, Checkout checkout) {
-		String tcId = getBook().getTravelComponentId();
+    
+    @Override
+    @BeforeMethod(alwaysRun = true)
+    @Parameters("environment")
+    public void setup(String environment) {
+        setEnvironment(environment);
+        
+        setUseDvcResort(true);
+        setCheckingIn(true);
+        setBook(bookDvcReservation("testBook_MCash", 1));
+        setTpId(getFirstBooking().getTravelPlanId());
+    }
+    
+    public String validateResMgmt(String TcId, Checkout checkout) {
+		String tcId = getFirstBooking().getTravelComponentId();
 
 		TestReporter.logStep("Verify Res Mgmt");
 		String sql = "select c.* " + " from res_mgmt.tps a "
 				+ " left outer join res_mgmt.tc_grp b on a.tps_id = b.tps_id "
 				+ " left outer join res_mgmt.tc c on b.tc_grp_nb = c.tc_grp_nb " + " where tc_id = "
-				+ getBook().getTravelComponentId();
+				+ getFirstBooking().getTravelComponentId();
+
+		Sleeper.sleep(15000);
+
 		Database db = new OracleDatabase(environment, Database.DREAMS);
 		Recordset rs = new Recordset(db.getResultSet(sql));
 
@@ -63,7 +65,7 @@ public class checkout_WDTC extends AccommodationBaseTest {
 				+ " From rsrc_inv.RSRC_ASGN_OWNR " + " Where ASGN_OWNR_ID = " + assignOwnerId;
 		Database db = new OracleDatabase(environment, Database.DREAMS);
 		Recordset rs = new Recordset(db.getResultSet(sql));
-		rs.print();
+		// rs.print();
 
 		for (int i = 1; i <= rs.getRowCount(); i++) {
 			if (rs.getValue("ASGN_OWNR_ID", i).equals(assignOwnerId)) {
@@ -84,14 +86,14 @@ public class checkout_WDTC extends AccommodationBaseTest {
 				+ " left outer join folio.CHRG_ITEM e on d.CHRG_ID = e.CHRG_ID"
 				+ " left outer join folio.CHRG_GRP_FOLIO f on c.CHRG_GRP_ID = f.ROOT_CHRG_GRP_ID "
 				+ " left outer join folio.FOLIO g on f.CHRG_GRP_FOLIO_ID = g.FOLIO_ID " + " where a.EXTNL_REF_VAL in ('"
-				+ getBook().getTravelPlanId() + "','" + getBook().getTravelPlanSegmentId() + "','"
-				+ getBook().getTravelComponentGroupingId() + "')" +
+				+ getFirstBooking().getTravelPlanId() + "','" + getFirstBooking().getTravelPlanSegmentId() + "','"
+				+ getFirstBooking().getTravelComponentGroupingId() + "')" +
 				// "where a.EXTNL_REF_VAL in
 				// ('472059709301','472051342254','472052990544')" +
 				" and folio_sts_nm is not null ";
 		Database db = new OracleDatabase(environment, Database.DREAMS);
 		Recordset rs = new Recordset(db.getResultSet(sql));
-		rs.print();
+		// rs.print();
 		String folioStat = "Earned";
 		for (int i = 1; i <= rs.getRowCount(); i++) {
 			if (rs.getValue("FOLIO_STS_NM", i).equals(folioStat)) {
@@ -101,34 +103,44 @@ public class checkout_WDTC extends AccommodationBaseTest {
 			}
 		}
 	}
-
 	@Test(groups = { "api", "regression", "checkout", "Accommodation" })
-	public void TestCheckout_wdtc() {
+	public void TestCheckout_roomOnly_DVC() {
+		
+		TestReporter.logScenario("Test Book DVC");	
+	    TestReporter.log("Travel Plan ID: " + book.get().getTravelPlanId());
+		TestReporter.assertNotNull(book.get().getTravelPlanId(), "The response contains a Travel Plan ID");
+		TestReporter.assertNotNull(book.get().getTravelPlanSegmentId(), "The response contains a Travel Plan Segment ID");
 
-		helper = new CheckInHelper(getEnvironment(), getBook());
-		helper.checkIn(getLocationId(), getDaysOut(), getNights(), getFacilityId());
-
-		String tcgId = getBook().getTravelComponentGroupingId();
-		String checkoutDate = Randomness.generateCurrentXMLDate();
-		String refNumber = getExternalRefNumber();
-		String refSource = getExternalRefSource();
-
+		helper = new CheckInHelper(getEnvironment(), getFirstBooking());
+        helper.checkIn(getLocationId(), getDaysOut(), getNights(), getFacilityId());String status = "false";
+		String tcgId = getFirstBooking().getTravelComponentGroupingId();
+		String locationId = getLocationId();
+		String checkoutDate = Randomness.generateCurrentXMLDate(); 
+		String earlyCheckoutReason = "BEREAV";
+		String refType = "MEMBERSHIP";
+		String refNumber = getFirstMember().getMembershipRefId();
+		String refSource = "DVC";
+		
 		Checkout checkout = new Checkout(getEnvironment(), "main");
-		checkout.setEarlyCheckOutReason(BaseSoapCommands.REMOVE_NODE.toString());
-		checkout.setIsBellServiceRequired(BaseSoapCommands.REMOVE_NODE.toString());
-		checkout.setIsSameRoomNumberAssigned(BaseSoapCommands.REMOVE_NODE.toString());
+		checkout.setEarlyCheckOutReason(earlyCheckoutReason);
+		checkout.setIsBellServiceRequired(status);
+		checkout.setIsSameRoomNumberAssigned(status);
 		checkout.setTravelComponentGroupingId(tcgId);
-		checkout.setExternalReferenceType(BaseSoapCommands.REMOVE_NODE.toString());
+		checkout.setExternalReferenceType(refType);
 		checkout.setExternalReferenceNumber(refNumber);
 		checkout.setExternalReferenceSource(refSource);
 		checkout.setExternalReferenceCode(BaseSoapCommands.REMOVE_NODE.toString());
 		checkout.setCheckoutDate(checkoutDate);
-		checkout.setLocationId(BaseSoapCommands.REMOVE_NODE.toString());
+		checkout.setLocationId(locationId);
 		checkout.sendRequest();
-
-		String assignOwnerId = validateResMgmt(getBook().getTravelComponentId(), checkout);
-		validateRIM(assignOwnerId, checkout);
-		validateFolio(getBook().getTravelComponentGroupingId(), checkout);
+        TestReporter.logAPI(!checkout.getResponseStatusCode().equals("200"), "Verify that no error occurred checking out a reservation: " + checkout.getFaultString(), checkout);
+		
+        //Validations 
+        String assignOwnerId =  validateResMgmt(getFirstBooking().getTravelComponentId(), checkout);		    
+				validateRIM(assignOwnerId, checkout);
+				validateFolio(getFirstBooking().getTravelComponentGroupingId(), checkout);
+		        
+        ValidationHelper backEndHelper = new ValidationHelper(getEnvironment());
+        backEndHelper.validateModificationBackend(1, "Past Visit", "DVC", getArrivalDate(), getDepartureDate(), "", "", getFirstBooking().getTravelPlanId(), getFirstBooking().getTravelPlanSegmentId(), getFirstBooking().getTravelComponentGroupingId(), false);
 	}
-
 }
