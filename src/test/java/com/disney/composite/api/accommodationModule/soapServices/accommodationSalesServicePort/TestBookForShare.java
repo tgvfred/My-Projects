6c060f456f77b0ef874f1fa5c5b9a50ff634d6d3
averw003 <waightstill.w.avery.-nd@disney.com>
2017-07-28 14:07:19 -0400

    removing non-essential tests from being included in the suite

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/TestBookForShare.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/TestBookForShare.java
@@ -12,41 +12,42 @@ import com.disney.utils.Randomness;
 import com.disney.utils.TestReporter;
 
 public class TestBookForShare {
-	private String environment = "";
-	private Book book = null;
-	
-	@BeforeMethod(alwaysRun = true)
-	@Parameters({"environment" })
-	public void setup(String environment) {
-		this.environment = environment;
-		book= new Book(environment, "bookRoomOnly2Adults2ChildrenWithoutTickets" );
-		book.sendRequest();
-	}
-	
-	@AfterMethod(alwaysRun=true)
-	public void teardown(){
-		try{
-			if(book != null){
-				if(book.getTravelPlanSegmentId() != null){
-					if(!book.getTravelPlanSegmentId().isEmpty()){
-						Cancel cancel = new Cancel(environment, "Main");
-						cancel.setCancelDate(Randomness.generateCurrentXMLDate(0));
-						cancel.setTravelComponentGroupingId(book.getTravelComponentGroupingId());
-						cancel.sendRequest();
-					}
-				}
-			}
-		}catch(Exception e){}
-	}
-		
-	@Test(groups={"api", "regression", "accommodation", "accommodationSalesService", "bookForShare"})
-	public void testBookForShare_MainFlow(){
-		TestReporter.logScenario("Test Book for Share");
-	    BookForShare BookForShare = new BookForShare(environment, "Main" );
-	    BookForShare.sendRequest();
-	    TestReporter.logAPI(!BookForShare.getResponseStatusCode().equals("200"), "An error occurred booking a reservation for share.", BookForShare);
-	    TestReporter.log("Travel Plan ID: " + book.getTravelPlanId());
-		TestReporter.assertNotNull(BookForShare.getlocationId(),  "The response contains a Location Id");
-		TestReporter.assertNotNull(BookForShare.getshared(),  "The response contains a Shared");
-	}
+    private String environment = "";
+    private Book book = null;
+
+    @BeforeMethod(alwaysRun = true)
+    @Parameters({ "environment" })
+    public void setup(String environment) {
+        this.environment = environment;
+        book = new Book(environment, "bookRoomOnly2Adults2ChildrenWithoutTickets");
+        book.sendRequest();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void teardown() {
+        try {
+            if (book != null) {
+                if (book.getTravelPlanSegmentId() != null) {
+                    if (!book.getTravelPlanSegmentId().isEmpty()) {
+                        Cancel cancel = new Cancel(environment, "Main");
+                        cancel.setCancelDate(Randomness.generateCurrentXMLDate(0));
+                        cancel.setTravelComponentGroupingId(book.getTravelComponentGroupingId());
+                        cancel.sendRequest();
+                    }
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    @Test(groups = { "api", "regression", "accommodation", "accommodationSalesService", "bookForShare", "example" })
+    public void testBookForShare_MainFlow() {
+        TestReporter.logScenario("Test Book for Share");
+        BookForShare BookForShare = new BookForShare(environment, "Main");
+        BookForShare.sendRequest();
+        TestReporter.logAPI(!BookForShare.getResponseStatusCode().equals("200"), "An error occurred booking a reservation for share.", BookForShare);
+        TestReporter.log("Travel Plan ID: " + book.getTravelPlanId());
+        TestReporter.assertNotNull(BookForShare.getlocationId(), "The response contains a Location Id");
+        TestReporter.assertNotNull(BookForShare.getshared(), "The response contains a Shared");
+    }
 }
--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/TestOverrideRate.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/TestOverrideRate.java
@@ -12,43 +12,44 @@ import com.disney.utils.Randomness;
 import com.disney.utils.TestReporter;
 
 public class TestOverrideRate {
-	private String environment = "";
+    private String environment = "";
     private Book book = null;
-    
+
     @BeforeMethod(alwaysRun = true)
-	@Parameters({  "environment" })
-	public void setup(String environment) {
-		this.environment = environment;
-		book= new Book(environment, "bookRoomOnly2Adults2ChildrenWithoutTickets" );
-		book.sendRequest();
-	}
-	
-	@AfterMethod(alwaysRun=true)
-	public void teardown(){
-		try{
-			if(book != null){
-				if(book.getTravelPlanSegmentId() != null){
-					if(!book.getTravelPlanSegmentId().isEmpty()){
-						Cancel cancel = new Cancel(environment, "Main");
-						cancel.setCancelDate(Randomness.generateCurrentXMLDate(0));
-						cancel.setTravelComponentGroupingId(book.getTravelComponentGroupingId());
-						cancel.sendRequest();
-					}
-				}
-			}
-		}catch(Exception e){}
-	}
-		
-	@Test(groups={"api", "regression", "accommodation", "accommodationSalesService", "overrideRate", "debug"})
-	public void testOverrideRate_MainFlow(){
-		TestReporter.logScenario("Test Override Rate");
-		OverrideRate OverrideRate= new OverrideRate(environment, "Main" );
-		OverrideRate.setTravelPlanSegementId(book.getTravelPlanSegmentId());
-		OverrideRate.setTravelComponentGroupingId(book.getTravelComponentGroupingId());
-		OverrideRate.sendRequest();
-		TestReporter.logAPI(!OverrideRate.getResponseStatusCode().equals("200"), "An error occurred overriding rate", OverrideRate);
-	    TestReporter.log("Travel Plan ID: " + book.getTravelPlanId());
-		TestReporter.assertNotNull(OverrideRate.gettravelComponentId(),  "The response contains a Travel Component Id");
-		TestReporter.assertNotNull(OverrideRate.gettravelComponentGroupingId(),  "The response contains a TravelComponent GroupingI d");
-   }
+    @Parameters({ "environment" })
+    public void setup(String environment) {
+        this.environment = environment;
+        book = new Book(environment, "bookRoomOnly2Adults2ChildrenWithoutTickets");
+        book.sendRequest();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void teardown() {
+        try {
+            if (book != null) {
+                if (book.getTravelPlanSegmentId() != null) {
+                    if (!book.getTravelPlanSegmentId().isEmpty()) {
+                        Cancel cancel = new Cancel(environment, "Main");
+                        cancel.setCancelDate(Randomness.generateCurrentXMLDate(0));
+                        cancel.setTravelComponentGroupingId(book.getTravelComponentGroupingId());
+                        cancel.sendRequest();
+                    }
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    @Test(groups = { "api", "regression", "accommodation", "accommodationSalesService", "overrideRate", "example" })
+    public void testOverrideRate_MainFlow() {
+        TestReporter.logScenario("Test Override Rate");
+        OverrideRate OverrideRate = new OverrideRate(environment, "Main");
+        OverrideRate.setTravelPlanSegementId(book.getTravelPlanSegmentId());
+        OverrideRate.setTravelComponentGroupingId(book.getTravelComponentGroupingId());
+        OverrideRate.sendRequest();
+        TestReporter.logAPI(!OverrideRate.getResponseStatusCode().equals("200"), "An error occurred overriding rate", OverrideRate);
+        TestReporter.log("Travel Plan ID: " + book.getTravelPlanId());
+        TestReporter.assertNotNull(OverrideRate.gettravelComponentId(), "The response contains a Travel Component Id");
+        TestReporter.assertNotNull(OverrideRate.gettravelComponentGroupingId(), "The response contains a TravelComponent GroupingI d");
+    }
 }
