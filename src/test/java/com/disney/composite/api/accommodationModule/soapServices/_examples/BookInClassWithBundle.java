62d4e2f848f42d8d7a0fc5c86cde3f708b0298bd
averw003 <waightstill.w.avery.-nd@disney.com>
2017-07-31 07:24:33 -0400

    helpers

---
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/_examples/BookInClassWithBundle.java
@@ -0,0 +1,36 @@
+package com.disney.composite.api.accommodationModule.soapServices._examples;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
+
+public class BookInClassWithBundle extends AccommodationBaseTest {
+
+    @Override
+    @BeforeMethod(alwaysRun = true)
+    @Parameters("environment")
+    public void setup(String environment) {
+        setEnvironment(environment);
+    }
+
+    @Override
+    @AfterMethod(alwaysRun = true)
+    public void teardown() {
+        cancel();
+    }
+
+    @Test(groups = { "api", "regression", "accommodation", "example" })
+    public void bookInClassAndCancelInTestClass() {
+        setDaysOut(0);
+        setNights(1);
+        setArrivalDate(getDaysOut());
+        setDepartureDate(getDaysOut() + getNights());
+        setValues(getEnvironment());
+        setIsBundle(true);
+        bookReservation();
+        cancel();
+    }
+}
