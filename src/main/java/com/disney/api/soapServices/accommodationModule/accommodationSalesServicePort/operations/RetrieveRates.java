14f17568885c80a388e92e028c3771f881b94f3f
Malic012 <Carey.A.Malika.-ND@disney.com>
2017-08-01 08:35:56 -0400

    updated operation class

--- src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesServicePort/operations/RetrieveRates.java
+++ src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesServicePort/operations/RetrieveRates.java
@@ -1,6 +1,8 @@
 package com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.operations;
 
 import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.AccommodationSalesServicePort;
+import com.disney.api.soapServices.core.BaseSoapCommands;
+import com.disney.api.soapServices.core.exceptions.XPathNotFoundException;
 import com.disney.utils.XMLTools;
 
 public class RetrieveRates extends AccommodationSalesServicePort{
@@ -24,8 +26,26 @@ public class RetrieveRates extends AccommodationSalesServicePort{
 	    public String getadditionalChargeOverridden(){
 		    return getResponseNodeValueByXPath("/Envelope/Body/retrieveRatesResponse/accomodationRatesDetail/ratedetails/additionalChargeOverridden");
 	    }
-	    public String getRate(){
+	    /*public String getRate(){
 	    return getResponseNodeValueByXPath("/Envelope/Body/retrieveRatesResponse/accomodationRatesDetail/ratedetails/rackRate/rate");
+	    }*/
+	    
+	   // int index2 = getNumberOfRequestNodesByXPath("/Envelope/Body/retrieveRatesResponse/accomodationRatesDetail/ratedetails/rackRate") + 1;
+	    
+	    public void setRequestTravelComponentGroupingIdIndex(String index3, String tcgId){
+			setRequestNodeValueByXPath("/Envelope/Body/retrieveRates/travelComponentGroupingId["+index3+"]", tcgId);
 	    }
+	  
+	 //Getters
+	    int index = getNumberOfRequestNodesByXPath("/Envelope/Body/retrieveRatesResponse/accomodationRatesDetail") + 1;
+		   public String getAccommodationRateDetails(String index) {return getResponseNodeValueByXPath("/Envelope/Body/retrieveRatesResponse/accomodationRatesDetail["+index+"]");}
+		   public Integer getAccommodationRateDetails(){return getNumberOfResponseNodesByXPath("/Envelope/Body/retrieveRatesResponse/accomodationRatesDetail");}
+	    
+	   int index1 = getNumberOfRequestNodesByXPath("/Envelope/Body/retrieveRatesResponse/accomodationRatesDetail/ratedetails") + 1;
+	   public String getRateDetails(String index) {return getResponseNodeValueByXPath("/Envelope/Body/retrieveRatesResponse/accomodationRatesDetail/ratedetails["+index+"]");}
+	   public Integer getRateDetails(){return getNumberOfResponseNodesByXPath("/Envelope/Body/retrieveRatesResponse/accomodationRatesDetail/ratedetails");}
+	   public String getRate(){
+		    return getResponseNodeValueByXPath("/Envelope/Body/retrieveRatesResponse/accomodationRatesDetail/ratedetails/rackRate/rate");
+		    }
 	    
 }
