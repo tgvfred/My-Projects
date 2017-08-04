742d6cb4c5621e1171965f1b3328b3134fe59413
Malic012 <Carey.A.Malika.-ND@disney.com>
2017-08-04 08:41:36 -0400

    nullProccessId

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationBatchComponentWSPort/retrieveIdsToProcess/TestRetreiveIdsToProcess_nullProcessId.java
+++
@@ -1,5 +0,0 @@
-package com.disney.composite.api.accommodationModule.soapServices.accommodationBatchComponentWSPort.retrieveIdsToProcess;
-
-public class TestRetreiveIdsToProcess_nullProcessId {
-
-}
