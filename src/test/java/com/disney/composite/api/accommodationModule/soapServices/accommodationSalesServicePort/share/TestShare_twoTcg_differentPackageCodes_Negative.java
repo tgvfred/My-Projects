f0a99b46e31d7f7baa5f085d5e3d6afaa9f85f0d
tayla215 <alissa.j.taylor.-ND@disney.com>
2017-08-08 09:19:35 -0400

    debug

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/share/TestShare_twoTcg_differentPackageCodes_Negative.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/share/TestShare_twoTcg_differentPackageCodes_Negative.java
@@ -4,9 +4,12 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.operations.ReplaceAllForTravelPlanSegment;
 import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.operations.Share;
 import com.disney.api.soapServices.accommodationModule.applicationError.AccommodationErrorCode;
 import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
+import com.disney.utils.Environment;
+import com.disney.utils.PackageCodes;
 import com.disney.utils.Randomness;
 import com.disney.utils.TestReporter;
 
@@ -14,21 +17,34 @@ public class TestShare_twoTcg_differentPackageCodes_Negative extends Accommodati
 
     private Share share;
     String firstTCG;
+    private ReplaceAllForTravelPlanSegment book, book1;
 
+    @Override
     @BeforeMethod(alwaysRun = true)
     @Parameters("environment")
     public void setup(String environment) {
         // TestReporter.setDebugLevel(TestReporter.INFO); //Uncomment this line
         // to invoke lower levels of reporting
+        Environment.getBaseEnvironmentName(environment);
         setEnvironment(environment);
         daysOut.set(0);
         nights.set(1);
         arrivalDate.set(Randomness.generateCurrentXMLDate(getDaysOut()));
         departureDate.set(Randomness.generateCurrentXMLDate(getDaysOut() + getNights()));
         setValues();
-        getBook().setRoomDetailsPackageCode(getPackageCode());
-        firstTCG = getBook().getTravelComponentGroupingId();
         bookReservation();
+
+        book = new ReplaceAllForTravelPlanSegment(environment, "RoomOnlyNoTickets");
+        book.sendRequest();
+
+        firstTCG = getBook().getTravelComponentGroupingId();
+
+        book1 = new ReplaceAllForTravelPlanSegment(environment, "RoomOnlyNoTickets");
+        PackageCodes pkg = new PackageCodes();
+        String packageCode = pkg.retrievePackageCode(getEnvironment(), String.valueOf(getDaysOut()),
+                getLocationId(), "RSR", "", getResortCode(), getRoomTypeCode(), "");
+        getBook().setRoomDetailsPackageCode(packageCode);
+        book1.sendRequest();
     }
 
     @Test(groups = { "api", "regression", "accommodation", "accommodationSalesService", "share", "negative" })
