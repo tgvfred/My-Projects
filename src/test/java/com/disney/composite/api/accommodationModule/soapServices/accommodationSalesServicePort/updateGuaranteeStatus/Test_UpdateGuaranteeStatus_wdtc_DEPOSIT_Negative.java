99a7fdecd4e7166b78f7f1b1b85708f8474e1594
DETSB001 <Billy.F.Detschel.-ND@disney.com>
2017-08-20 09:49:41 -0400

    Negative Scenarios

---
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/updateGuaranteeStatus/Test_UpdateGuaranteeStatus_wdtc_DEPOSIT_Negative.java
@@ -0,0 +1,53 @@
+package com.disney.composite.api.accommodationModule.soapServices.accommodationSalesServicePort.updateGuaranteeStatus;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.operations.UpdateGuaranteeStatus;
+import com.disney.api.soapServices.accommodationModule.applicationError.AccommodationErrorCode;
+import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
+import com.disney.utils.TestReporter;
+
+public class Test_UpdateGuaranteeStatus_wdtc_DEPOSIT_Negative extends AccommodationBaseTest {
+
+    private String environment;
+
+    @Override
+    @BeforeMethod(alwaysRun = true)
+    @Parameters("environment")
+    public void setup(String environment) {
+        this.environment = environment;
+
+    }
+
+    @Override
+    @AfterMethod(alwaysRun = true)
+    public void teardown() {
+        cancel();
+    }
+
+    @Test(groups = { "api", "regression", "accommodation", "accommodationSalesService", "UpdateGuaranteeStatus", "negative" })
+    public void testUpdateGuaranteeStatus_wdtc_DEPOSIT_Negative() {
+
+        String faultString = " Guarantee status can not be changed : guarantee status can not be changed";
+
+        setDaysOut(0);
+        setNights(1);
+        setArrivalDate(getDaysOut());
+        setDepartureDate(getDaysOut() + getNights());
+        setValues();
+        setIsWdtcBooking(true);
+        bookReservation();
+
+        UpdateGuaranteeStatus update = new UpdateGuaranteeStatus(environment);
+        update.setRequestTravelComponentGroupingId(getBook().getTravelComponentGroupingId());
+        update.setRequestguaranteedByEnum("DEPOSIT");
+        update.sendRequest();
+
+        TestReporter.assertEquals(faultString, update.getFaultString(), "Verify that the fault string [" + update.getFaultString() + "] is that which is expected.[" + faultString + "]");
+        validateApplicationError(update, AccommodationErrorCode.GUARANTEE_STATUS_CANNOT_CHANGE);
+    }
+
+}
