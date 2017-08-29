82fe4a1a35aed862a6a77ae1d2c23ed6e24fb174
portc073 <Colton.P.Porter.-ND@disney.com>
2017-08-29 12:24:25 -0400

    fixing cancel

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/cancel/TestCancel_RO_Shared.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/cancel/TestCancel_RO_Shared.java
@@ -4,44 +4,39 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.operations.Book;
 import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.operations.Cancel;
 import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.operations.Share;
+import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
 import com.disney.api.soapServices.accommodationModule.helpers.CancelHelper;
 import com.disney.api.soapServices.core.exceptions.XPathNotFoundException;
-import com.disney.api.soapServices.travelPlanModule.TravelPlanBaseTest;
 import com.disney.utils.Environment;
-import com.disney.utils.PackageCodes;
-import com.disney.utils.Randomness;
 import com.disney.utils.TestReporter;
 import com.disney.utils.XMLTools;
-import com.disney.utils.dataFactory.guestFactory.HouseHold;
 import com.disney.utils.date.DateTimeConversion;
 
-public class TestCancel_RO_Shared extends TravelPlanBaseTest {
-
-    private Book book;
-    private HouseHold hh;
-    private String packageCode = "";
-    private static final int maxTries = 3;
+public class TestCancel_RO_Shared extends AccommodationBaseTest {
 
     @Override
     @BeforeMethod(alwaysRun = true)
     @Parameters("environment")
     public void setup(String environment) {
-        this.environment = environment;
-        if (environment.toLowerCase().contains("_cm")) {
-            environment = environment.toLowerCase().replace("_cm", "");
-        }
-        book = book();
-        hh = getHouseHold();
+        setEnvironment(environment);
+        setDaysOut(0);
+        setNights(1);
+        setArrivalDate(getDaysOut());
+        setDepartureDate(getNights());
+        setValues(getEnvironment());
+        isComo.set("true");
+        // setIsShared(true);
+
+        bookReservation();
 
         Share share = new Share(environment.toLowerCase().replace("_cm", ""));
-        share.setTravelComponentGroupingId(book.getTravelComponentGroupingId());
+        share.setTravelComponentGroupingId(getBook().getTravelComponentGroupingId());
         share.setRequestNodeValueByXPath("/Envelope/Body/share/request/roomNumber", "fx:removenode");
         share.setRequestNodeValueByXPath("/Envelope/Body/share/request/locationId", getLocationId());
         share.sendRequest();
-        TestReporter.assertTrue(share.getResponseStatusCode().equals("200"), "Verify that no error occurred sharing TCG ID [" + book.getTravelComponentGroupingId() + "]: " + share.getFaultString());
+        TestReporter.assertTrue(share.getResponseStatusCode().equals("200"), "Verify that no error occurred sharing TCG ID [" + getBook().getTravelComponentGroupingId() + "]: " + share.getFaultString());
     }
 
     @Test(groups = { "api", "regression", "accommodation", "accommodationSalesService", "Cancel" })
@@ -50,7 +45,7 @@ public class TestCancel_RO_Shared extends TravelPlanBaseTest {
 
         Cancel cancel = new Cancel(environment, "Main");
         cancel.setCancelDate(DateTimeConversion.ConvertToDateYYYYMMDD("0"));
-        cancel.setTravelComponentGroupingId(book.getTravelComponentGroupingId());
+        cancel.setTravelComponentGroupingId(getBook().getTravelComponentGroupingId());
         cancel.sendRequest();
         TestReporter.logAPI(!cancel.getResponseStatusCode().equals("200"), "An error occurred cancelling the reservation: " + cancel.getFaultString(), cancel);
         TestReporter.assertNotNull(cancel.getCancellationNumber(), "The response contains a cancellation number");
@@ -98,14 +93,14 @@ public class TestCancel_RO_Shared extends TravelPlanBaseTest {
     }
 
     private void validations(Cancel cancel) {
-        CancelHelper cancelHelper = new CancelHelper(removeCM(environment), book.getTravelPlanId());
+        CancelHelper cancelHelper = new CancelHelper(removeCM(environment), getBook().getTravelPlanId());
         cancelHelper.verifyChargeGroupsCancelled();
-        cancelHelper.verifyCancellationIsFoundInResHistory(book.getTravelPlanSegmentId(), book.getTravelComponentGroupingId(), book.getTravelComponentId());
+        cancelHelper.verifyCancellationIsFoundInResHistory(getBook().getTravelPlanSegmentId(), getBook().getTravelComponentGroupingId(), getBook().getTravelComponentId());
         // cancelHelper.verifyCancellationComment(getRetrieve(), "Air not available CancellationNumber : " + cancel.getCancellationNumber());
         cancelHelper.verifyNumberOfCharges(1);
-        cancelHelper.verifyInventoryReleased(book.getTravelComponentGroupingId());
+        cancelHelper.verifyInventoryReleased(getBook().getTravelComponentGroupingId());
         cancelHelper.verifyNumberOfTpPartiesByTpId(1);
-        cancelHelper.verifyTcStatusByTcg(book.getTravelComponentGroupingId(), "Cancelled");
+        cancelHelper.verifyTcStatusByTcg(getBook().getTravelComponentGroupingId(), "Cancelled");
         cancelHelper.verifyExchangeFeeFound(false);
         cancelHelper.verifyChargeGroupsStatusCount("Cancelled", 2);
         cancelHelper.verifyChargeGroupsStatusCount("UnEarned", 0);
@@ -113,93 +108,14 @@ public class TestCancel_RO_Shared extends TravelPlanBaseTest {
         cancelHelper.verifyNumberOfChargesByStatus("UnEarned", 0);
         // Verify the reasonID matches the reason code used for the given TCId
         // cancelHelper.verifyProductReasonID(book.getTravelComponentId());
-        cancelHelper.verifyTPV3GuestRecordCreated(book.getTravelPlanId(), getHouseHold().primaryGuest());
-        cancelHelper.verifyTPV3RecordCreated(book.getTravelPlanId());
-        cancelHelper.verifyTPV3SalesOrderRecordCreated(book.getTravelPlanId());
+        cancelHelper.verifyTPV3GuestRecordCreated(getBook().getTravelPlanId(), getHouseHold().primaryGuest());
+        cancelHelper.verifyTPV3RecordCreated(getBook().getTravelPlanId());
+        cancelHelper.verifyTPV3SalesOrderRecordCreated(getBook().getTravelPlanId());
         TestReporter.assertAll();
     }
 
     public static String removeCM(String cmEnv) {
         return Environment.getBaseEnvironmentName(cmEnv).toLowerCase();
-    }
-
-    private Book book() {
-        createHouseHold();
-        Book book = new Book(environment.toLowerCase().replace("_cm", ""), "bookWithoutTickets");
-        int locDaysOut = 0;
-        book.setArrivalDate(String.valueOf(locDaysOut));
-        int locNights = 1;
-        book.setDeptDate(String.valueOf(String.valueOf(locDaysOut + locNights)));
-
-        boolean bookSuccess = false;
-        int tries = 0;
-        int maxTries = 10;
-        PackageCodes pkg = new PackageCodes();
-        do {
-            setValues("80010402", "EP", "42");
-
-            pkg = new PackageCodes();
-            boolean success = false;
-            // String packageCode = "";
-            do {
-                try {
-                    packageCode = pkg.retrievePackageCode(environment.toLowerCase().replace("_cm", ""), String.valueOf(locDaysOut),
-                            getLocationId(), "DRC RO", "", getResortCode(), getRoomTypeCode(), "");
-                    success = true;
-                } catch (AssertionError e) {
-                    if (!getNoPackageCodes().containsKey(getResortCode() + ":" + getLocationId() + ":" + getRoomTypeCode())) {
-                        String message = "No package code found for resort[" + getResortCode() + "], locationId[" + getLocationId() + "], and roomType[" + getRoomTypeCode() + "]:";
-                        addToNoPackageCodes(getResortCode() + ":" + getLocationId() + ":" + getRoomTypeCode(), message);
-                    }
-                    setValues("80010402", "EP", "43");
-                }
-            } while (!success);
-
-            book.setPackageCode(packageCode);
-            book.setResortCode(getResortCode());
-            book.setRoomTypeCode(getRoomTypeCode());
-            book.setLocationID(getLocationId());
-            book.setPrimaryGuestFirstName(getHouseHold().primaryGuest().getFirstName());
-            book.setPrimaryGuestFirstNameGuestRefDetails(getHouseHold().primaryGuest().getFirstName());
-            book.setPrimaryGuestFirstNameTravelPlan(getHouseHold().primaryGuest().getFirstName());
-            book.setPrimaryGuestLastName(getHouseHold().primaryGuest().getLastName());
-            book.setPrimaryGuestLastNameGuestRefDetails(getHouseHold().primaryGuest().getLastName());
-            book.setPrimaryGuestLastNameTravelPlan(getHouseHold().primaryGuest().getLastName());
-            book.setRequestNodeValueByXPath("//book/request/roomDetail/roomReservationDetail/guestReferenceDetails/guest/addressDetails/addressLine1", getHouseHold().primaryGuest().primaryAddress().getAddress1());
-            book.setRequestNodeValueByXPath("//book/request/roomDetail/roomReservationDetail/guestReferenceDetails/guest/addressDetails/city", getHouseHold().primaryGuest().primaryAddress().getCity());
-            book.setRequestNodeValueByXPath("//book/request/roomDetail/roomReservationDetail/guestReferenceDetails/guest/addressDetails/regionName", getHouseHold().primaryGuest().primaryAddress().getState());
-            book.setRequestNodeValueByXPath("//book/request/roomDetail/roomReservationDetail/guestReferenceDetails/guest/addressDetails/state", getHouseHold().primaryGuest().primaryAddress().getState());
-            book.setRequestNodeValueByXPath("//book/request/roomDetail/roomReservationDetail/guestReferenceDetails/guest/addressDetails/postalCode", getHouseHold().primaryGuest().primaryAddress().getZipCode());
-            book.setRequestNodeValueByXPath("//book/request/travelPlanGuest/addressDetails/addressLine1", getHouseHold().primaryGuest().primaryAddress().getAddress1());
-            book.setRequestNodeValueByXPath("//book/request/travelPlanGuest/addressDetails/city", getHouseHold().primaryGuest().primaryAddress().getCity());
-            book.setRequestNodeValueByXPath("//book/request/travelPlanGuest/addressDetails/postalCode", getHouseHold().primaryGuest().primaryAddress().getZipCode());
-            book.setRequestNodeValueByXPath("//book/request/travelPlanGuest/addressDetails/state", getHouseHold().primaryGuest().primaryAddress().getState());
-            book.setRequestNodeValueByXPath("//book/request/travelPlanGuest/addressDetails/regionName", getHouseHold().primaryGuest().primaryAddress().getState());
-            book.setPhoneNumber(getHouseHold().primaryGuest().primaryPhone().getNumber());
-            book.setEmail(getHouseHold().primaryGuest().primaryEmail().getEmail());
-
-            book.setRequestNodeValueByXPath("/Envelope/Body/book/request/travelPlanGuest/phoneDetails/number", getHouseHold().primaryGuest().getAllPhones().get(0).getNumber());
-            book.setRequestNodeValueByXPath("/Envelope/Body/book/request/travelPlanGuest/emailDetails/address", getHouseHold().primaryGuest().getAllEmails().get(0).getEmail());
-
-            book.sendRequest();
-            if (book.getResponseStatusCode().equals("200")) {
-                bookSuccess = true;
-            } else {
-                tries++;
-            }
-        } while (!bookSuccess && tries < maxTries);
-        TestReporter.assertTrue(book.getResponseStatusCode().equals("200"), "Verify that no error occurred booking the prereq reservation: " + book.getFaultString());
-        setTpId(book.getTravelPlanId());
-        setTpsId(book.getTravelPlanSegmentId());
-        setTcgId(book.getTravelComponentGroupingId());
-        setTcId(book.getTravelComponentId());
-        setArrivalDate(Randomness.generateCurrentXMLDate(locDaysOut));
-        setDepartureDate(String.valueOf(Randomness.generateCurrentXMLDate(locDaysOut + locNights)));
-        retrieveReservation(environment.toLowerCase().replace("_cm", ""));
-        makeFirstNightDeposit(environment.toLowerCase().replace("_cm", ""));
-        retrieveReservation(environment.toLowerCase().replace("_cm", ""));
-
-        return book;
     }
 
 }
