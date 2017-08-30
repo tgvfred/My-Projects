1c99895e99812aa6b40eecf5ac9d6d6961990276
jackj248 <joshua.c.jackson.-nd@disney.com>
2017-08-30 15:11:54 -0400

    updating parent ID test

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/updateComments/TestUpdateComments_parentTC_parentIdOnly.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/updateComments/TestUpdateComments_parentTC_parentIdOnly.java
@@ -26,8 +26,9 @@ public class TestUpdateComments_parentTC_parentIdOnly extends AccommodationBaseT
         update.setProfileId(BaseSoapCommands.REMOVE_NODE.toString());
         update.setProfileCode(BaseSoapCommands.REMOVE_NODE.toString());
         update.setCommentId(BaseSoapCommands.REMOVE_NODE.toString());
-        update.setcommentText(BaseSoapCommands.REMOVE_NODE.toString());
-        update.setCommentLevel(BaseSoapCommands.REMOVE_NODE.toString());
+        update.setcommentText(commentText);
+        update.setCommentLevel("TC");
+        update.setRequestNodeValueByXPath("/Envelope/Body/updateComments/request/commentsInfo/commentType", BaseSoapCommands.REMOVE_NODE.toString());
         update.setRequestNodeValueByXPath("/Envelope/Body/updateComments/request/commentsInfo/auditDetail", BaseSoapCommands.REMOVE_NODE.toString());
         update.setRequestNodeValueByXPath("/Envelope/Body/updateComments/request/commentsInfo/commentOwnerDetail", BaseSoapCommands.REMOVE_NODE.toString());
         update.setRequestNodeValueByXPath("/Envelope/Body/updateComments/request/roomExternalReference", BaseSoapCommands.REMOVE_NODE.toString());
