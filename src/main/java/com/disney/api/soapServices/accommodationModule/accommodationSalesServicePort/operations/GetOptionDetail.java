827ac398fa280d0f91eda7e7ce8747c396d62c2e
DAWKJ004 <Jade.M.Dawkins.-ND@disney.com>
2017-09-06 08:19:21 -0400

    adding branch getOptionDetail

--- src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesServicePort/operations/GetOptionDetail.java
+++ src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesServicePort/operations/GetOptionDetail.java
@@ -3,18 +3,18 @@ package com.disney.api.soapServices.accommodationModule.accommodationSalesServic
 import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.AccommodationSalesServicePort;
 import com.disney.utils.XMLTools;
 
-public class GetOptionDetail extends AccommodationSalesServicePort{
-	public GetOptionDetail(String environment, String scenario) {
-		super(environment);
+public class GetOptionDetail extends AccommodationSalesServicePort {
+    public GetOptionDetail(String environment, String scenario) {
+        super(environment);
 
-		//Generate a request from a project xml file
-		setRequestDocument(XMLTools.loadXML(buildRequestFromWSDL("getOptionDetail")));
-		generateServiceContext();
-	    setRequestNodeValueByXPath(getTestScenario(getService(), getOperation(), scenario));
-		removeComments() ;
-		removeWhiteSpace();
+        // Generate a request from a project xml file
+        setRequestDocument(XMLTools.loadXML(buildRequestFromWSDL("getOptionDetail")));
+        generateServiceContext();
+        setRequestNodeValueByXPath(getTestScenario(getService(), getOperation(), scenario));
+        removeComments();
+        removeWhiteSpace();
 
-	}
-	
-	
+    }
+
 }
+// going to add to this
