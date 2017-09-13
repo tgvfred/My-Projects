23c12bc7b773d6a53f35ef158dd386418e40157f
Tony.James <Tony.C.James.-ND@disney.com>
2017-09-13 16:41:57 -0400

    added validations

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/retrieveComments/TestRetrieveComments_roomOnly_commentWithProfileId.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/retrieveComments/TestRetrieveComments_roomOnly_commentWithProfileId.java
@@ -33,7 +33,7 @@ public class TestRetrieveComments_roomOnly_commentWithProfileId extends Accommod
         create.setIsActive(expectedIsActive);
         create.setSendToGSR(expectedGSR);
         create.setConfidential(expectedConfidential);
-        create.setProfileId(BaseSoapCommands.REMOVE_NODE.toString());
+        create.setProfileId("1065");
         create.setProfileCode(BaseSoapCommands.REMOVE_NODE.toString());
         create.setCommentId(BaseSoapCommands.REMOVE_NODE.toString());
         create.setCommentText(commentText);
@@ -69,54 +69,7 @@ public class TestRetrieveComments_roomOnly_commentWithProfileId extends Accommod
         validate(create, retrieve);
         validateActiveOrder(retrieve);
 
-        // Validate comment data in RES_MGMT_REQ table
-        String GSR_IN = (create.getSendToGSR().equals("true")) ? "Y" : "N";
-        String CFDNTL_IN = (create.getConfidential().equals("true")) ? "Y" : "N";
-
-        String RES_MGMT_REQ_VALIDATE_sql = "SELECT * " +
-                " FROM RES_MGMT.RES_MGMT_REQ " +
-                " WHERE TC_ID = " + parentId + " " +
-                " AND GSR_IN = '" + GSR_IN + "' " +
-                " AND CFDNTL_IN = '" + CFDNTL_IN + "' " +
-                " AND RES_MGMT_REQ_TX = '" + create.getCommentText() + "' ";
-
-        Database RES_MGMT_REQ_VALIDATE_db = new OracleDatabase(environment, Database.DREAMS);
-        Recordset RES_MGMT_REQ_VALIDATE_rs = new Recordset(RES_MGMT_REQ_VALIDATE_db.getResultSet(RES_MGMT_REQ_VALIDATE_sql));
-
-        TestReporter.logStep("Verify that the comment shows up in the RES_MGMT_REQ_VALIDATE database.");
-        TestReporter.setAssertFailed(false);
-        TestReporter.softAssertEquals(RES_MGMT_REQ_VALIDATE_rs.getValue("TC_ID"), parentId, "Verify that the RES_MGMT_VAIDATE data [ " + RES_MGMT_REQ_VALIDATE_rs.getValue("TC_ID") + "] matches the comment data [ " + parentId + "]");
-        TestReporter.softAssertEquals(RES_MGMT_REQ_VALIDATE_rs.getValue("RES_MGMT_REQ_TX"), create.getCommentText(), "Verify that the RES_MGMT_VAIDATE data [ " + RES_MGMT_REQ_VALIDATE_rs.getValue("RES_MGMT_REQ_TX") + "] matches the comment data [ " + create.getCommentText() + "]");
-        TestReporter.softAssertEquals(RES_MGMT_REQ_VALIDATE_rs.getValue("GSR_IN"), GSR_IN, "Verify that the RES_MGMT_VAIDATE data [ " + RES_MGMT_REQ_VALIDATE_rs.getValue("GSR_IN") + "] matches the comment data [ " + GSR_IN + "]");
-        TestReporter.softAssertEquals(RES_MGMT_REQ_VALIDATE_rs.getValue("CFDNTL_IN"), CFDNTL_IN, "Verify that the RES_MGMT_VAIDATE data [ " + RES_MGMT_REQ_VALIDATE_rs.getValue("CFDNTL_IN") + "] matches the comment data [ " + CFDNTL_IN + "]");
-        TestReporter.assertAll();
-
-        // GSR WILL NO LONGER BE INVOKED POST COMO. PLEASE SEE TASK TK-635536 FOR DETAILS
-        String sql = "select * " +
-                " from ext_intf.gsr_rcd a " +
-                " join ext_intf.gsr_guest b on a.GSR_GUEST_ID = b.GSR_GUEST_ID " +
-                " where a.tps_id = '" + getBook().getTravelPlanSegmentId() + "'" +
-                " and a.cmt_tx = '" + create.getCommentText() + "' ";
-
-        Database db = new OracleDatabase(environment, Database.DREAMS);
-        Recordset rs = new Recordset(db.getResultSet(sql));
-
-        TestReporter.logStep("Verify that no GSR records are returned");
-        TestReporter.softAssertEquals(rs.getRowCount(), 0, "Verify that no GSR records were returned.");
-
-        // Verify that the comment exists in the TPV3 database table
-        String TPV3_sql = "select * " +
-                " from sales_tp.tp " +
-                " where tp_id = '" + getBook().getTravelPlanId() + "' ";
-
-        Database TPV3_db = new OracleDatabase(environment, Database.SALESTP);
-        Recordset TPV3_rs = new Recordset(TPV3_db.getResultSet(TPV3_sql));
-
-        TestReporter.logStep("Verify that the comment shows up in the TPV3 database.");
-        TestReporter.setAssertFailed(false);
-        TestReporter.softAssertEquals(TPV3_rs.getValue("TP_ID"), getBook().getTravelPlanId(), "Verify that the TPV3 databse tp id [ " + TPV3_rs.getValue("TP_ID") + "] matches the comment data [ " + getBook().getTravelPlanId() + "]");
-        TestReporter.assertAll();
-        
+     
         
         if (Environment.isSpecialEnvironment(environment)) {
 
@@ -140,6 +93,7 @@ public class TestRetrieveComments_roomOnly_commentWithProfileId extends Accommod
         for (int i = 1; i <= retrieve.getNumberOfResponseNodesByXPath("/Envelope/Body/retrieveCommentsResponse/response/commentsInfo"); i++) {
             String commentXPath = "/Envelope/Body/retrieveCommentsResponse/response/commentsInfo[" + i + "]/";
             if (create.getCommentId().equals(retrieve.getResponseNodeValueByXPath(commentXPath + "commentId"))) {
+                                
                 TestReporter.softAssertEquals(create.getIsActive(), retrieve.getResponseNodeValueByXPath(commentXPath + "isActive"), "Verify that the retrieved isActive node [" + retrieve.getResponseNodeValueByXPath(commentXPath + "isActive") + "] matches the expected [" + create.getIsActive() + "]");
                 TestReporter.softAssertEquals(create.getSendToGSR(), retrieve.getResponseNodeValueByXPath(commentXPath + "sendToGSR"), "Verify that the retrieved sendToGSR node [" + retrieve.getResponseNodeValueByXPath(commentXPath + "sendToGSR") + "] matches the expected [" + create.getSendToGSR() + "]");
                 TestReporter.softAssertEquals(create.getConfidential(), retrieve.getResponseNodeValueByXPath(commentXPath + "confidential"), "Verify that the retrieved confidential node [" + retrieve.getResponseNodeValueByXPath(commentXPath + "confidential") + "] matches the expected [" + create.getConfidential() + "]");
@@ -147,6 +101,7 @@ public class TestRetrieveComments_roomOnly_commentWithProfileId extends Accommod
                 TestReporter.softAssertEquals(create.getCommentText(), retrieve.getResponseNodeValueByXPath(commentXPath + "commentText"), "Verify that the retrieved commentText node [" + retrieve.getResponseNodeValueByXPath(commentXPath + "commentText") + "] matches the expected [" + create.getCommentText() + "]");
                 TestReporter.softAssertEquals(create.getCommentLevel(), retrieve.getResponseNodeValueByXPath(commentXPath + "commentLevel"), "Verify that the retrieved commentLevel node [" + retrieve.getResponseNodeValueByXPath(commentXPath + "commentLevel") + "] matches the expected [" + create.getCommentLevel() + "]");
                 TestReporter.softAssertEquals(create.getCreatedBy(), retrieve.getResponseNodeValueByXPath(commentXPath + "auditDetail/createdBy"), "Verify that the retrieved createdBy node [" + retrieve.getResponseNodeValueByXPath(commentXPath + "auditDetail/createdBy") + "] matches the expected [" + create.getCreatedBy() + "]");
+                TestReporter.softAssertEquals(create.getProfileId(), retrieve.getResponseNodeValueByXPath(commentXPath + "profileId"), "Verify that the retrieved profileID node [" + retrieve.getResponseNodeValueByXPath(commentXPath + "profileId") + "] matches the expected [" + create.getProfileId() + "]");
                 String[] createdDate = create.getCreatedDate().split("T");
                 String[] retrievedDate = retrieve.getResponseNodeValueByXPath(commentXPath + "auditDetail/createdDate").split("T");
                 TestReporter.softAssertEquals(createdDate[0], retrievedDate[0], "Verify that the retrieved createdDate node [" + retrievedDate[0] + "] matches the expected [" + createdDate[0] + "]");
