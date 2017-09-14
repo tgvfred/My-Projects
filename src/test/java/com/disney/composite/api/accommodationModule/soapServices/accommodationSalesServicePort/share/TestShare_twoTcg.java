c52794d9cccbe6ab40a266d44627355e0e2dcc58
DETSB001 <Billy.F.Detschel.-ND@disney.com>
2017-09-14 09:33:54 -0400

    Share/Unshare

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/share/TestShare_twoTcg.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/share/TestShare_twoTcg.java
@@ -1,6 +1,5 @@
 package com.disney.composite.api.accommodationModule.soapServices.accommodationSalesServicePort.share;
 
-import org.testng.SkipException;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -53,11 +52,11 @@ public class TestShare_twoTcg extends AccommodationBaseTest {
 
     @Test(groups = { "api", "regression", "accommodation", "accommodationSalesService", "share" })
     public void Test_Share_twoTcg() {
-        if (Environment.isSpecialEnvironment(environment)) {
-            if (true) {
-                throw new SkipException("Folio Fix in Progress, for now operation not supported.");
-            }
-        }
+        // if (Environment.isSpecialEnvironment(environment)) {
+        // if (true) {
+        // throw new SkipException("Folio Fix in Progress, for now operation not supported.");
+        // }
+        // }
         captureSecondOwnerId();
 
         // verify that the owner id's for the first and second tcg do not match.
--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/unShare/TestUnShare_emptyUnSharedComponents_Negative.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/unShare/TestUnShare_emptyUnSharedComponents_Negative.java
@@ -1,13 +1,11 @@
 package com.disney.composite.api.accommodationModule.soapServices.accommodationSalesServicePort.unShare;
 
-import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.operations.UnShare;
 import com.disney.api.soapServices.accommodationModule.applicationError.LiloResmErrorCode;
 import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
 import com.disney.api.soapServices.core.BaseSoapCommands;
-import com.disney.utils.Environment;
 import com.disney.utils.TestReporter;
 
 public class TestUnShare_emptyUnSharedComponents_Negative extends AccommodationBaseTest {
@@ -16,11 +14,11 @@ public class TestUnShare_emptyUnSharedComponents_Negative extends AccommodationB
 
     @Test(groups = { "api", "regression", "accommodation", "accommodationSalesService", "unShare", "negative" })
     public void Test_unShare_emptyUnSharedComponents_Negative() {
-        if (Environment.isSpecialEnvironment(environment)) {
-            if (true) {
-                throw new SkipException("Folio Fix in Progress, for now operation not supported.");
-            }
-        }
+        // if (Environment.isSpecialEnvironment(environment)) {
+        // if (true) {
+        // throw new SkipException("Folio Fix in Progress, for now operation not supported.");
+        // }
+        // }
         unshare = new UnShare(environment, "Main");
         unshare.setTravelComponentGroupingId(BaseSoapCommands.REMOVE_NODE.toString());
         unshare.sendRequest();
