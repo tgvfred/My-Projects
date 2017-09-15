0bc3094c759c10fc988c9a4b57984b4f017827d4
jackj248 <joshua.c.jackson.-nd@disney.com>
2017-09-15 08:08:46 -0400

    committing changes

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationBatchWSPort/retrieveAvailableFields/TestRetrieveAvailableFields.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationBatchWSPort/retrieveAvailableFields/TestRetrieveAvailableFields.java
@@ -1,6 +1,11 @@
 package com.disney.composite.api.accommodationModule.soapServices.accommodationBatchWSPort.retrieveAvailableFields;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import org.testng.annotations.Test;
+import org.w3c.dom.NodeList;
 
 import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
 import com.disney.api.soapServices.accommodationModule.roomingListServicePort.operation.RetrieveAvailableFields;
@@ -28,8 +33,6 @@ public class TestRetrieveAvailableFields extends AccommodationBaseTest {
 
         String xpath = "/Envelope/Body/retrieveAvaillableFieldsResponse/fieldTOs";
         int numOfFieldTOs = retrieve.getNumberOfResponseNodesByXPath(xpath);
-        int expectedNumofItems = 5;
-        int numOfChildNodes = XMLTools.getNodeList(retrieve.getResponseDocument(), xpath).item(0).getChildNodes().getLength();
 
         // Verify the number of fieldTOs in the response is the same as the query recordset
         TestReporter.logStep("Verify the number of fieldTOs in the response is the same as the query recordset");
@@ -51,12 +54,115 @@ public class TestRetrieveAvailableFields extends AccommodationBaseTest {
         }
 
         // Verify that there are no extra nodes in the response that are not in the database
+        int expectedNumofItems = 5;
+        int numOfChildNodes = XMLTools.getNodeList(retrieve.getResponseDocument(), xpath).item(0).getChildNodes().getLength();
+        // Make a map of the API fieldIds
+        Map<String, String> api = makeApiMap(childNodes(xpath, retrieve));
+        // Make a copy of the map of the API fieldIds
+        Map<String, String> tempApi = new HashMap<>();
+        tempApi.putAll(api);
+
+        // Make a map of the DB fieldIds
+        Map<String, String> db2 = new HashMap<>();
+        // Make a copy of the map of the DB fieldIds
+        Map<String, String> tempDb = new HashMap<>();
+        tempDb.putAll(db2);
+
+        // Determine API vs DB
+        Set<String> apiKeySet = api.keySet();
+        for (String key : apiKeySet) {
+            if (tempDb.containsKey(key)) {
+                tempDb.remove(key);
+            }
+        }
+        TestReporter.assertTrue(tempDb.isEmpty(), "Verify that the DB does not have any extraneous fields.");
+        tempDb.putAll(db2);
+
+        // Determine DB vs API
+        Set<String> dbKeySet = db2.keySet();
+        for (String key : dbKeySet) {
+            if (tempApi.containsKey(key)) {
+                tempApi.remove(key);
+            }
+        }
+        TestReporter.assertTrue(tempApi.isEmpty(), "Verify that the API does not have any extraneous fields.");
+        tempApi.putAll(api);
+
+        // System.out.println(childNodes);
+
         TestReporter.logStep("Verify that there are no extra nodes in the response that are not in the database");
         compare(numOfChildNodes, expectedNumofItems);
 
         // Verify that there are no extra records in the database that are not in the response
         TestReporter.logStep("Verify that there are no extra records in the database that are not in the response");
         compare(rs.getColumnCount(), expectedNumofItems);
+    }
+
+    public NodeList childNodes(String xpath, RetrieveAvailableFields retrieve) {
+        NodeList childNodes = XMLTools.getNodeList(retrieve.getResponseDocument(), xpath);// .item(0).getChildNodes();
+        return childNodes;
+    }
+
+    public Map<String, String> makeApiMap(NodeList childNodes) {
+        Map<String, String> api = new HashMap<>();
+
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            for (int j = 0; j < childNodes.item(i).getChildNodes().getLength(); j++) {
+                if (childNodes.item(i).getChildNodes().item(j).getNodeName().equals("fieldId")) {
+                    api.put(childNodes.item(i).getChildNodes().item(j).getTextContent(), childNodes.item(i).getChildNodes().item(j).getTextContent());
+                    break;
+                }
+                if (childNodes.item(i).getChildNodes().item(j).getNodeName().equals("isRepeatable")) {
+                    api.put(childNodes.item(i).getChildNodes().item(j).getTextContent(), childNodes.item(i).getChildNodes().item(j).getTextContent());
+                    break;
+                }
+                if (childNodes.item(i).getChildNodes().item(j).getNodeName().equals("defaultFieldIndicator")) {
+                    api.put(childNodes.item(i).getChildNodes().item(j).getTextContent(), childNodes.item(i).getChildNodes().item(j).getTextContent());
+                    break;
+                }
+                if (childNodes.item(i).getChildNodes().item(j).getNodeName().equals("fieldName")) {
+                    api.put(childNodes.item(i).getChildNodes().item(j).getTextContent(), childNodes.item(i).getChildNodes().item(j).getTextContent());
+                    break;
+                }
+                if (childNodes.item(i).getChildNodes().item(j).getNodeName().equals("fieldDataType")) {
+                    api.put(childNodes.item(i).getChildNodes().item(j).getTextContent(), childNodes.item(i).getChildNodes().item(j).getTextContent());
+                    break;
+                }
+            }
+        }
+
+        return api;
+    }
+
+    public Map<String, String> makeApiMap(Recordset rs) {
+        Map<String, String> db2 = new HashMap<>();
+
+        for (int i = 0; i < rs.getRowCount(); i++) {
+            for (int j = 0; j < rs.getColumnCount(); j++) {
+                if (rs.getValue(i, j).equals("FLD_ID")) {
+                    db2.put(rs.getValue(i, j), rs.getValue(i, j));
+                    break;
+                }
+                if (rs.getValue(i, j).equals("REPEAT_FLD_IN")) {
+                    db2.put(rs.getValue(i, j), rs.getValue(i, j));
+                    break;
+                }
+                if (rs.getValue(i, j).equals("DFLT_FLD_IN")) {
+                    db2.put(rs.getValue(i, j), rs.getValue(i, j));
+                    break;
+                }
+                if (rs.getValue(i, j).equals("FLD_NM")) {
+                    db2.put(rs.getValue(i, j), rs.getValue(i, j));
+                    break;
+                }
+                if (rs.getValue(i, j).equals("FLD_DATA_TYP_NM")) {
+                    db2.put(rs.getValue(i, j), rs.getValue(i, j));
+                    break;
+                }
+            }
+        }
+
+        return db2;
     }
 
     public void compare(String a, String b) {
