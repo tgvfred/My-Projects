4637bf7f9daf938ee92701695bd92269a5589b90
tayla215 <alissa.j.taylor.-ND@disney.com>
2017-09-15 10:37:50 -0400

    main class/test dev

--- src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesServicePort/operations/CalculateUnsharedRates.java
+++ src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesServicePort/operations/CalculateUnsharedRates.java
@@ -3,20 +3,56 @@ package com.disney.api.soapServices.accommodationModule.accommodationSalesServic
 import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.AccommodationSalesServicePort;
 import com.disney.utils.XMLTools;
 
-public class CalculateUnsharedRates extends AccommodationSalesServicePort{
+public class CalculateUnsharedRates extends AccommodationSalesServicePort {
     public CalculateUnsharedRates(String environment, String scenario) {
-		super(environment);
-		//Generate a request from a project xml file
-		setRequestDocument(XMLTools.loadXML(buildRequestFromWSDL("calculateUnsharedRates")));
-		generateServiceContext();
-	    setRequestNodeValueByXPath(getTestScenario(getService(), getOperation(), scenario));
-        removeComments() ;
-		removeWhiteSpace(); 
+        super(environment);
+        // Generate a request from a project xml file
+        setRequestDocument(XMLTools.loadXML(buildRequestFromWSDL("calculateUnsharedRates")));
+        setRequestNodeValueByXPath(getTestScenario(getService(), getOperation(), scenario));
+        removeComments();
+        removeWhiteSpace();
+        generateServiceContext();
     }
-    public String getfreezeId(){
-	   return getResponseNodeValueByXPath("/Envelope/Body/calculateUnsharedRatesResponse/splitRateWithTotalTO/shareChains/shareRoomDetails/unSharedRoomDetail/freezeId");
+
+    // setters
+    public void setUnsharedChainUnsharedRoomDetailTCGId(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/calculateUnsharedRates/request/unSharedChain/shareRoomDetails/unSharedRoomDetail/travelComponentGroupingId", value);
     }
-    public String getinventoryStatus(){
-	   return getResponseNodeValueByXPath("/Envelope/Body/calculateUnsharedRatesResponse/splitRateWithTotalTO/shareChains/shareRoomDetails/unSharedRoomDetail/inventoryStatus");
+
+    public void setUnsharedChainUnsharedRoomDetailTCId(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/calculateUnsharedRates/request/unSharedChain/shareRoomDetails/unSharedRoomDetail/travelComponentId", value);
+    }
+
+    public void setUnsharedChainSharedRoomDetailTCGId(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/calculateUnsharedRates/request/unSharedChain/shareRoomDetails/sharedRoomDetail/travelComponentGroupingId", value);
+    }
+
+    public void setUnsharedChainSharedRoomDetailTCId(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/calculateUnsharedRates/request/unSharedChain/shareRoomDetails/sharedRoomDetail/travelComponentId", value);
+    }
+
+    public void setUnsharedChainShareRoomDetailsTPSId(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/calculateUnsharedRates/request/unSharedChain/shareRoomDetails/travelPlanSegmentId", value);
+    }
+
+    public void setUnsharedAccommodationUnShareRoomDetailsTCGId(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/calculateUnsharedRates/request/unsharedAccomadation/unSharedRoomDetail/travelComponentGroupingId", value);
+    }
+
+    public void setUnsharedAccommodationUnShareRoomDetailsTCId(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/calculateUnsharedRates/request/unsharedAccomadation/unSharedRoomDetail/travelComponentId", value);
+    }
+
+    public void setUnsharedAccommodationTPSd(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/calculateUnsharedRates/request/unsharedAccomadation/travelPlanSegmentId", value);
+    }
+
+    // getters
+    public String getfreezeId() {
+        return getResponseNodeValueByXPath("/Envelope/Body/calculateUnsharedRatesResponse/splitRateWithTotalTO/shareChains/shareRoomDetails/unSharedRoomDetail/freezeId");
+    }
+
+    public String getinventoryStatus() {
+        return getResponseNodeValueByXPath("/Envelope/Body/calculateUnsharedRatesResponse/splitRateWithTotalTO/shareChains/shareRoomDetails/unSharedRoomDetail/inventoryStatus");
     }
 }
---
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/calculateUnsharedRates/TestCalculateUnsharedRates_OneSharedRoomDetails.java
@@ -0,0 +1,17 @@
+package com.disney.composite.api.accommodationModule.soapServices.accommodationSalesServicePort.calculateUnsharedRates;
+
+import org.testng.annotations.Test;
+
+import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.operations.CalculateUnsharedRates;
+import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
+
+public class TestCalculateUnsharedRates_OneSharedRoomDetails extends AccommodationBaseTest {
+
+    CalculateUnsharedRates calculate;
+
+    @Test(groups = { "api", "regression", "accommodation", "accommodationSalesService", "calculateUnsharedRates" })
+    public void Test_CalculateUnsharedRates_OneSharedRoomDetails() {
+
+    }
+
+}
