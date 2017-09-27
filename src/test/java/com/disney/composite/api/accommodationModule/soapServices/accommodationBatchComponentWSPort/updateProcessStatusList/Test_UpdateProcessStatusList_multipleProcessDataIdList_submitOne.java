810ab12d3d3a9b8d2556eaa1b68e16b2becf4174
DETSB001 <Billy.F.Detschel.-ND@disney.com>
2017-09-27 07:46:13 -0400

    Mods

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationBatchComponentWSPort/updateProcessStatusList/Test_UpdateProcessStatusList_multipleProcessDataIdList_submitOne.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationBatchComponentWSPort/updateProcessStatusList/Test_UpdateProcessStatusList_multipleProcessDataIdList_submitOne.java
@@ -81,6 +81,6 @@ public class Test_UpdateProcessStatusList_multipleProcessDataIdList_submitOne ex
 
         TestReporter.logStep("Verify Proc Run ID that was not passed into the RQ [" + helper.retrieveProcRunIdMulti2(cancel.getResponseProcessId()) + "] doesn't have an updated status:");
         // Sleeper.sleep(10000);
-        helper.validationOverall(helper.retrieveProcRunIdMulti2(cancel.getResponseProcessId()), "SUBMITTED", Randomness.generateCurrentDatetime().substring(0, 10));
+        helper.validationOverall(helper.retrieveProcRunIdMulti2(cancel.getResponseProcessId()), "BOOKED", Randomness.generateCurrentDatetime().substring(0, 10));
     }
 }
