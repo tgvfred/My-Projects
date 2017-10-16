ae2ad73bbb26bc3f0c3f9bbb30be2dba9d10f70b
portc073 <Colton.P.Porter.-ND@disney.com>
2017-10-16 08:49:43 -0400

    workin retPostedCancelFee

---
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/retrievePostedCancellationFee/TestRetrievePostedCancellationFee_nullIdentityDetails.java
@@ -0,0 +1,9 @@
+package com.disney.composite.api.accommodationModule.soapServices.accommodationSalesServicePort.retrievePostedCancellationFee;
+
+import org.testng.annotations.Test;
+
+public class TestRetrievePostedCancellationFee_nullIdentityDetails {
+  @Test
+  public void f() {
+  }
+}
