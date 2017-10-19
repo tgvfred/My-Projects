c65ad2b05c3fda2713026833703c819be7f57001
phlej001 <justin.x.phlegar.-nd@disney.com>
2017-10-19 17:05:09 -0400

    Fleshing out Accommodation Batch svc API test needs

---
+++ src/test/java/com/disney/composite/api/accommodationModule/restServices/accommodation/accommodationBatchService/job/post/processMain/Template.java
@@ -0,0 +1,10 @@
+package com.disney.composite.api.accommodationModule.restServices.accommodation.accommodationBatchService.job.post.processMain;
+
+import com.disney.api.restServices.BaseRestTest;
+
+public class Template extends BaseRestTest {
+
+    public void sandbox() {
+
+    }
+}
