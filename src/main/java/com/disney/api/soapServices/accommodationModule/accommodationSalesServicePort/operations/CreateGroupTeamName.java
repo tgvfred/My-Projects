476dfe6b3c60d6fc03219a18fa72f98c30387bca
tayla215 <alissa.j.taylor.-ND@disney.com>
2017-10-19 10:31:48 -0400

    main class dev

--- src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesServicePort/operations/CreateGroupTeamName.java
+++ src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesServicePort/operations/CreateGroupTeamName.java
@@ -3,39 +3,68 @@ package com.disney.api.soapServices.accommodationModule.accommodationSalesServic
 import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.AccommodationSalesServicePort;
 import com.disney.utils.XMLTools;
 
-public class CreateGroupTeamName extends AccommodationSalesServicePort{
-	
-	public CreateGroupTeamName(String environment, String scenario) {
-		super(environment);
-		
-		//Generate a request from a project xml file
-		setRequestDocument(XMLTools.loadXML(buildRequestFromWSDL("createGroupTeamName")));
-        generateServiceContext();			
-	    setRequestNodeValueByXPath(getTestScenario(getService(), getOperation(), scenario));
-	    removeComments() ;
-		removeWhiteSpace();
-	}
-	public void setgroupcode(String code){
-	setRequestNodeValueByXPath("/Envelope/Body/createGroupTeamName/request/groupCode",code);
-    }
-    public void setgroupTeamViewTO(String code){
-	setRequestNodeValueByXPath("/Envelope/Body/createGroupTeamName/request/groupTeamViewTO/groupCode",code);
-    }
-    public void setgroupTeamName(String code){
-    setRequestNodeValueByXPath("/Envelope/Body/createGroupTeamName/request/groupTeamViewTO/groupTeamName",code);
-    }
-    
+public class CreateGroupTeamName extends AccommodationSalesServicePort {
 
-	public String getGroupCode(){
-		return getResponseNodeValueByXPath("/Envelope/Body/createGroupTeamNameResponse/response/groupCode");
-	}
-	
-	public String getGroupTeamId(){
-		return getResponseNodeValueByXPath("/Envelope/Body/createGroupTeamNameResponse/response/groupTeamId");
-	}
-	
-	public  String getGroupTeamName(){
-		return getResponseNodeValueByXPath("/Envelope/Body/createGroupTeamNameResponse/response/groupTeamName");
-	}
-	
+    public CreateGroupTeamName(String environment, String scenario) {
+        super(environment);
+
+        // Generate a request from a project xml file
+        setRequestDocument(XMLTools.loadXML(buildRequestFromWSDL("createGroupTeamName")));
+        generateServiceContext();
+        setRequestNodeValueByXPath(getTestScenario(getService(), getOperation(), scenario));
+        removeComments();
+        removeWhiteSpace();
+    }
+
+    // old setters
+    public void setgroupcode(String code) {
+        setRequestNodeValueByXPath("/Envelope/Body/createGroupTeamName/request/groupCode", code);
+    }
+
+    public void setgroupTeamViewTO(String code) {
+        setRequestNodeValueByXPath("/Envelope/Body/createGroupTeamName/request/groupTeamViewTO/groupCode", code);
+    }
+
+    public void setgroupTeamName(String code) {
+        setRequestNodeValueByXPath("/Envelope/Body/createGroupTeamName/request/groupTeamViewTO/groupTeamName", code);
+    }
+
+    // setters
+    public void setGroupCode(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/createGroupTeamName/request/groupCode", value);
+    }
+
+    public void setGroupTeamViewTOGroupCode(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/createGroupTeamName/request/groupTeamViewTO/groupCode", value);
+    }
+
+    public void setGroupName(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/createGroupTeamName/request/groupTeamViewTO/groupName", value);
+    }
+
+    public void setGroupTeamId(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/createGroupTeamName/request/groupTeamViewTO/groupTeamId", value);
+    }
+
+    public void setGroupTeamName(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/createGroupTeamName/request/groupTeamViewTO/groupTeamName", value);
+    }
+
+    public void setSelected(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/createGroupTeamName/request/groupTeamViewTO/selected", value);
+    }
+
+    // getters
+    public String getGroupCode() {
+        return getResponseNodeValueByXPath("/Envelope/Body/createGroupTeamNameResponse/response/groupCode");
+    }
+
+    public String getGroupTeamId() {
+        return getResponseNodeValueByXPath("/Envelope/Body/createGroupTeamNameResponse/response/groupTeamId");
+    }
+
+    public String getGroupTeamName() {
+        return getResponseNodeValueByXPath("/Envelope/Body/createGroupTeamNameResponse/response/groupTeamName");
+    }
+
 }
