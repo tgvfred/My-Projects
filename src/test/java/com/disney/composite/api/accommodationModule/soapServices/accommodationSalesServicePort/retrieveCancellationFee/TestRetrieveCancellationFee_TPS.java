33d0b5431b4dcaf41a5d78c6ca041ae826db5906
Malic012 <Carey.A.Malika.-ND@disney.com>
2017-10-20 13:00:30 -0400

    set up of TPS scenario

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/retrieveCancellationFee/TestRetrieveCancellationFee_TPS.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/retrieveCancellationFee/TestRetrieveCancellationFee_TPS.java
@@ -1,5 +1,30 @@
 package com.disney.composite.api.accommodationModule.soapServices.accommodationSalesServicePort.retrieveCancellationFee;
 
-public class TestRetrieveCancellationFee_TPS {
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
+import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
+
+public class TestRetrieveCancellationFee_TPS extends AccommodationBaseTest {
+
+    @Override
+    @Parameters("environment")
+    @BeforeMethod(alwaysRun = true)
+    public void setup(String environment) {
+        setEnvironment(environment);
+        isComo.set("false");
+        setDaysOut(0);
+        setNights(1);
+        setArrivalDate(getDaysOut());
+        setDepartureDate(getDaysOut() + getNights());
+        setValues(getEnvironment());
+        // locVar = environment;
+        bookReservation();
+    }
+
+    @Test(groups = { "api", "regression", "accommodation", "accommodationSalesService", "retrieveCancellationFee" })
+    public void testRetrieveCancellationFee_TPS() {
+
+    }
 }
