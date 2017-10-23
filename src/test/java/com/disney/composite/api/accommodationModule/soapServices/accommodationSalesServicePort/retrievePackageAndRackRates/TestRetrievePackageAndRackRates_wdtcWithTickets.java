c5fd042f06dc514f8f7fd868682bc428d208e98b
DAWKJ004 <Jade.M.Dawkins.-ND@disney.com>
2017-10-23 11:52:26 -0400

    adding files 10/23

---
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/retrievePackageAndRackRates/TestRetrievePackageAndRackRates_wdtcWithTickets.java
@@ -0,0 +1,83 @@
+package com.disney.composite.api.accommodationModule.soapServices.accommodationSalesServicePort.retrievePackageAndRackRates;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.operations.RetrievePackageAndRackRates;
+import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
+import com.disney.utils.Environment;
+import com.disney.utils.Sleeper;
+import com.disney.utils.TestReporter;
+
+public class TestRetrievePackageAndRackRates_wdtcWithTickets extends AccommodationBaseTest {
+    @Override
+    @BeforeMethod(alwaysRun = true)
+    @Parameters("environment")
+    public void setup(String environment) {
+        setEnvironment(environment);
+        isComo.set("false");
+        setDaysOut(0);
+        setNights(1);
+        setArrivalDate(getDaysOut());
+        setDepartureDate(getDaysOut() + getNights());
+        setValues(getEnvironment());
+        setIsWdtcBooking(true);
+        setAddTickets(true);
+
+        isComo.set("false");
+        bookReservation();
+
+    }
+
+    // test
+    @Test(groups = { "api", "regression", "accommodation", "accommodationSalesService", "retrievePackageAndRackRates" })
+    public void testRetrievePackageAndRackRates_wdtcWithTickets() {
+
+        RetrievePackageAndRackRates retrievePackage = new RetrievePackageAndRackRates(environment, "Main");
+        retrievePackage.setaccomComponentId("1239200667");
+        retrievePackage.setPackageCode(getPackageCode());
+        retrievePackage.setTravelPlanSegementId(getBook().getTravelPlanSegmentId());
+        retrievePackage.sendRequest();
+
+        TestReporter.logAPI(!retrievePackage.getResponseStatusCode().equals("200"), "An error occurred retrieving the package and rack rates [" + getBook().getTravelComponentGroupingId() + "]", retrievePackage);
+
+        // validations
+
+        if (Environment.isSpecialEnvironment(getEnvironment())) {
+
+            RetrievePackageAndRackRates clone = (RetrievePackageAndRackRates) retrievePackage.clone();
+            clone.setEnvironment(Environment.getBaseEnvironmentName(getEnvironment()));
+
+            int tries = 0;
+            int maxTries = 40;
+            boolean success = false;
+            tries = 0;
+            maxTries = 40;
+            success = false;
+            do {
+                Sleeper.sleep(500);
+                clone.sendRequest();
+                if (retrievePackage.getResponseStatusCode().equals("200")) {
+                    success = true;
+                } else {
+                    tries++;
+                }
+            } while (tries < maxTries && !success);
+            if (!clone.getResponseStatusCode().equals("200")) {
+                TestReporter.logAPI(!clone.getResponseStatusCode().equals("200"),
+                        "Error was returned: " + clone.getFaultString(), clone);
+            }
+            clone.addExcludedBaselineXpathValidations("/Envelope/Header");
+
+            clone.addExcludedBaselineXpathValidations("/Envelope/Body/retrievePackageAndRackRatesResponse/response/partyRoles/role");
+
+            clone.addExcludedXpathValidations("/Envelope/Body/retrievePackageAndRackRatesResponse/response/couponProducts[text()='2789372']");
+            clone.addExcludedXpathValidations("/Envelope/Body/retrievePackageAndRackRatesResponse/response/couponProducts[text()='2789375']");
+            clone.addExcludedXpathValidations("/Envelope/Body/retrievePackageAndRackRatesResponse/response/couponProducts[text()='2789376']");
+
+            TestReporter.assertTrue(clone.validateResponseNodeQuantity(retrievePackage, true), "Validating Response Comparison");
+
+        }
+    }
+}
