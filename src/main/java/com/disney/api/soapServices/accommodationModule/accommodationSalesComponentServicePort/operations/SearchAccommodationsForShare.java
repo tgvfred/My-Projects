a4d09c1a1616e70cd28fb55fca1bff10d2c59390
portc073 <Colton.P.Porter.-ND@disney.com>
2017-10-24 12:15:29 -0400

    dev tests for searchAccomForShare

--- src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesComponentServicePort/operations/SearchAccommodationsForShare.java
+++ src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesComponentServicePort/operations/SearchAccommodationsForShare.java
@@ -14,8 +14,50 @@ public class SearchAccommodationsForShare extends AccommodationSalesComponentSer
         setRequestNodeValueByXPath(getTestScenario(getService(), getOperation(), scenario));
         removeComments();
         removeWhiteSpace();
+        generateServiceContext();
     }
 
-    /Envelope/Body/searchAccommodationsForShare/request/travelPlanViewTO/arrivalDate
+    // Request
 
+    public void setArrivalDate(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/searchAccommodationsForShare/request/travelPlanViewTO/arrivalDate", value);
+    }
+
+    public void setDepatureDate(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/searchAccommodationsForShare/request/travelPlanViewTO/depatureDate", value);
+    }
+
+    public void setGuestFirstName(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/searchAccommodationsForShare/request/travelPlanViewTO/guestFirstName", value);
+    }
+
+    public void setGuestLastName(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/searchAccommodationsForShare/request/travelPlanViewTO/guestLastName", value);
+    }
+
+    public void setResortCode(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/searchAccommodationsForShare/request/travelPlanViewTO/resortCode", value);
+    }
+
+    public void setAccomFacTOResortCode(String value) {
+        setRequestNodeValueByXPath("Envelope/Body/searchAccommodationsForShare/request/travelPlanViewTO/roomType/accommodationFacilityTO/resortCode", value);
+    }
+
+    public void setRoomTypeCode(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/searchAccommodationsForShare/request/travelPlanViewTO/roomType/roomTypeCode", value);
+    }
+
+    public void setTravelPlanId(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/searchAccommodationsForShare/request/travelPlanViewTO/travelPlanId", value);
+    }
+
+    public void setTravelPlanSegmentId(String value) {
+        setRequestNodeValueByXPath("/Envelope/Body/searchAccommodationsForShare/request/travelPlanViewTO/travelPlanSegmentId", value);
+    }
+
+    // Response
+
+    public String getBookingDate() {
+        return getResponseNodeValueByXPath("/Envelope/Body/searchAccommodationsForShareResponse/return/unSharedRoomDetail/bookingDate");
+    }
 }
--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesComponentServicePort/searchAccommodationsForShare/Test_SearchAccommodationsForShare_ROSameDay_TPS_Positive.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesComponentServicePort/searchAccommodationsForShare/Test_SearchAccommodationsForShare_ROSameDay_TPS_Positive.java
@@ -2,8 +2,26 @@ package com.disney.composite.api.accommodationModule.soapServices.accommodationS
 
 import org.testng.annotations.Test;
 
-public class Test_SearchAccommodationsForShare_ROSameDay_TPS_Positive {
-  @Test
-  public void f() {
-  }
+import com.disney.api.soapServices.accommodationModule.accommodationSalesComponentServicePort.operations.SearchAccommodationsForShare;
+import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
+import com.disney.utils.TestReporter;
+
+public class Test_SearchAccommodationsForShare_ROSameDay_TPS_Positive extends AccommodationBaseTest {
+
+    @Test
+    public void test_SearchAccommodationsForShare_ROSameDay_TPS_Positive() {
+
+        SearchAccommodationsForShare search = new SearchAccommodationsForShare(environment, "Main");
+        search.setGuestFirstName(getHouseHold().primaryGuest().getFirstName());
+        search.setGuestLastName(getHouseHold().primaryGuest().getLastName());
+        search.setTravelPlanId(getBook().getTravelPlanId());
+        search.setTravelPlanSegmentId(getBook().getTravelPlanSegmentId());
+        search.setAccomFacTOResortCode(getResortCode());
+        search.setResortCode(getResortCode());
+        search.setRoomTypeCode(getRoomTypeCode());
+        search.sendRequest();
+
+        TestReporter.logAPI(!search.getResponseStatusCode().equals("200"), "An error occurred searching accommodation for share", search);
+
+    }
 }
