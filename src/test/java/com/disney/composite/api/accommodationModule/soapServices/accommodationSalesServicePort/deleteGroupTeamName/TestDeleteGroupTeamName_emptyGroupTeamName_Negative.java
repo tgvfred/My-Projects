0bfee3ef2832f5b7fec0c3dc7eccb08f8ec979b7
tayla215 <alissa.j.taylor.-ND@disney.com>
2017-10-24 15:51:00 -0400

    test issue

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/deleteGroupTeamName/TestDeleteGroupTeamName_emptyGroupTeamName_Negative.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/deleteGroupTeamName/TestDeleteGroupTeamName_emptyGroupTeamName_Negative.java
@@ -3,8 +3,9 @@ package com.disney.composite.api.accommodationModule.soapServices.accommodationS
 import org.testng.annotations.Test;
 
 import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.operations.DeleteGroupTeamName;
-import com.disney.api.soapServices.accommodationModule.applicationError.AccommodationErrorCode;
+import com.disney.api.soapServices.accommodationModule.applicationError.LiloResmErrorCode;
 import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
+import com.disney.utils.Environment;
 import com.disney.utils.TestReporter;
 
 public class TestDeleteGroupTeamName_emptyGroupTeamName_Negative extends AccommodationBaseTest {
@@ -12,9 +13,9 @@ public class TestDeleteGroupTeamName_emptyGroupTeamName_Negative extends Accommo
     DeleteGroupTeamName delete;
 
     @Test(groups = { "api", "regression", "deleteGroupTeamName", "accommodation", "accommodatoinsales", "negative" })
-    public void Test_DeleteGroupTeamName_nullGroupTeamName_Negative() {
+    public void Test_DeleteGroupTeamName_emptyGroupTeamName_Negative() {
 
-        delete = new DeleteGroupTeamName(environment, "_Main");
+        delete = new DeleteGroupTeamName(Environment.getBaseEnvironmentName(environment), "_Main");
         delete.setGroupName("Marbles2");
         delete.setgroupTeamName(" ");
         delete.setSelected("true");
@@ -22,7 +23,7 @@ public class TestDeleteGroupTeamName_emptyGroupTeamName_Negative extends Accommo
 
         String faultstring = "Invalid Delete Team Name Request : Invalid request. Mandatory fields not keyed in!";
 
-        validateApplicationError(delete, AccommodationErrorCode.DELETE_TEAM_NAME);
+        validateApplicationError(delete, LiloResmErrorCode.INVALID_DELETE_TEAM_REQUEST);
 
         TestReporter.assertEquals(faultstring, delete.getFaultString(), "Verify that the fault string [" + delete.getFaultString() + "] is that which is expected.[" + faultstring + "]");
 
