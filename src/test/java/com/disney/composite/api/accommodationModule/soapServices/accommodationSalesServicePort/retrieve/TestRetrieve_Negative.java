305c0827cff362420e7c251d7f42ce0d01514091
DAWKJ004 <Jade.M.Dawkins.-ND@disney.com>
2017-10-26 11:34:36 -0400

    adding negative test

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/retrieve/TestRetrieve_Negative.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/retrieve/TestRetrieve_Negative.java
@@ -1,5 +1,27 @@
 package com.disney.composite.api.accommodationModule.soapServices.accommodationSalesServicePort.retrieve;
 
+import org.testng.annotations.Test;
+
 public class TestRetrieve_Negative {
+
+    @Test(groups = { "api", "regression", "accommodation", "accommodationSalesService", "retrieve" })
+    public void testRetrieve_invalidLocationId() {
+
+    }
+
+    @Test(groups = { "api", "regression", "accommodation", "accommodationSalesService", "retrieve" })
+    public void testRetrieve_invalidTpAndTps() {
+
+    }
+
+    @Test(groups = { "api", "regression", "accommodation", "accommodationSalesService", "retrieve" })
+    public void testRetrieve_invalidTps() {
+
+    }
+
+    @Test(groups = { "api", "regression", "accommodation", "accommodationSalesService", "retrieve" })
+    public void testRetrieve_invalidTp() {
+
+    }
 
 }
