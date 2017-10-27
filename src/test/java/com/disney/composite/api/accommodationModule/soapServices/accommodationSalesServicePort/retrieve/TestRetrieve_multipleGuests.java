3ebe6f9fbbabac5542d50f15acf088c227c07f17
DAWKJ004 <Jade.M.Dawkins.-ND@disney.com>
2017-10-27 15:37:35 -0400

    updates

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/retrieve/TestRetrieve_multipleGuests.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/retrieve/TestRetrieve_multipleGuests.java
@@ -4,7 +4,10 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.operations.Retrieve;
 import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
+import com.disney.api.soapServices.accommodationModule.helpers.RetrieveHelper;
+import com.disney.utils.TestReporter;
 
 public class TestRetrieve_multipleGuests extends AccommodationBaseTest {
 
@@ -28,6 +31,17 @@ public class TestRetrieve_multipleGuests extends AccommodationBaseTest {
 
     @Test(groups = { "api", "regression", "accommodation", "accommodationSalesService", "retrieve" })
     public void testRetrieve_multipleGuests() {
+
+        Retrieve retrieve = new Retrieve(environment);
+        retrieve.setTravelPlanSegmentId(getBook().getTravelPlanSegmentId());
+        retrieve.setTravelPlanId(getBook().getTravelPlanId());
+        retrieve.setSiebelTravelPlanId("0");
+        retrieve.setLocationId(getLocationId());
+        retrieve.sendRequest();
+        TestReporter.logAPI(!retrieve.getResponseStatusCode().equals("200"), "An error occurred getting option details: " + retrieve.getFaultString(), retrieve);
+
+        RetrieveHelper helper = new RetrieveHelper();
+        helper.baseValidation(getBook(), retrieve);
 
     }
 
