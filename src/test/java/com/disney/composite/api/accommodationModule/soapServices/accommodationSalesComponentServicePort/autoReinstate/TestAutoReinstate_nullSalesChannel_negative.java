b6e1f4e4daea84f4387f75d8b071fd7f11ce6cf9
tayla215 <alissa.j.taylor.-ND@disney.com>
2017-11-02 16:17:35 -0400

    test design

---
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesComponentServicePort/autoReinstate/TestAutoReinstate_nullSalesChannel_negative.java
@@ -0,0 +1,5 @@
+package com.disney.composite.api.accommodationModule.soapServices.accommodationSalesComponentServicePort.autoReinstate;
+
+public class TestAutoReinstate_nullSalesChannel_negative {
+
+}
