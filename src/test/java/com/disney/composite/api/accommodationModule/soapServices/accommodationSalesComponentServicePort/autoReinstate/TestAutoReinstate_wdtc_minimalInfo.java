fe96911c6f0b181cbcd926974b42a71963c47efe
tayla215 <alissa.j.taylor.-ND@disney.com>
2017-11-06 16:12:48 -0500

    test design

---
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesComponentServicePort/autoReinstate/TestAutoReinstate_wdtc_minimalInfo.java
@@ -0,0 +1,7 @@
+package com.disney.composite.api.accommodationModule.soapServices.accommodationSalesComponentServicePort.autoReinstate;
+
+import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
+
+public class TestAutoReinstate_wdtc_minimalInfo extends AccommodationBaseTest {
+
+}
