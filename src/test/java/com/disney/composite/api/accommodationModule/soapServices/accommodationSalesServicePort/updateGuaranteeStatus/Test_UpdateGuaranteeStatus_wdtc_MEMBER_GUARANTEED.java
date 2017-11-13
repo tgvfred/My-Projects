0472ad6d804b2b480ba3457f6ef0e9f218b7c88a
DETSB001 <Billy.F.Detschel.-ND@disney.com>
2017-11-13 11:34:15 -0500

    updateGrntSts Updates

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/updateGuaranteeStatus/Test_UpdateGuaranteeStatus_wdtc_MEMBER_GUARANTEED.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesServicePort/updateGuaranteeStatus/Test_UpdateGuaranteeStatus_wdtc_MEMBER_GUARANTEED.java
@@ -8,7 +8,6 @@ import org.testng.annotations.Test;
 import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.operations.UpdateGuaranteeStatus;
 import com.disney.api.soapServices.accommodationModule.helpers.AccommodationBaseTest;
 import com.disney.api.soapServices.accommodationModule.helpers.UpdateGuaranteeStatusHelper;
-import com.disney.utils.Environment;
 import com.disney.utils.TestReporter;
 
 public class Test_UpdateGuaranteeStatus_wdtc_MEMBER_GUARANTEED extends AccommodationBaseTest {
@@ -53,14 +52,16 @@ public class Test_UpdateGuaranteeStatus_wdtc_MEMBER_GUARANTEED extends Accommoda
         update.sendRequest();
         TestReporter.logAPI(!update.getResponseStatusCode().equals("200"), "An error occurred retrieving the summary for the travel component grouping [" + getBook().getTravelComponentGroupingId() + "]", update);
 
-        if (Environment.isSpecialEnvironment(environment)) {
-            // Validation when running in Latest_CM
-            helper.validation(getBook().getTravelPlanId(), "MEMBER_GUARANTEED", "GROUP_GUARANTEED", "N");
-        } else {
-            // Validation when running in Latest
-            helper.validation(getBook().getTravelPlanId(), "DEPOSIT", "MEMBER_GUARANTEED", "N");
+        helper.validation(getBook().getTravelPlanId(), "DEPOSIT", "MEMBER_GUARANTEED", "N");
 
-        }
+        // if (Environment.isSpecialEnvironment(environment)) {
+        // // Validation when running in Latest_CM
+        // helper.validation(getBook().getTravelPlanId(), "MEMBER_GUARANTEED", "GROUP_GUARANTEED", "N");
+        // } else {
+        // // Validation when running in Latest
+        // helper.validation(getBook().getTravelPlanId(), "DEPOSIT", "MEMBER_GUARANTEED", "N");
+        //
+        // }
     }
 
 }
