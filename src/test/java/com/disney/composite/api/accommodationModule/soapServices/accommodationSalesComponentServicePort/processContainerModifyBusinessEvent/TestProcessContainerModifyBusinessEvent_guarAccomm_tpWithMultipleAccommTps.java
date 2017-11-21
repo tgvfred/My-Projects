7ca805e4b370fe6b525daf2f34bdc5cf65f0d2d5
averw003 <waightstill.w.avery.-nd@disney.com>
2017-11-21 07:18:59 -0500

    hardening

--- src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesComponentServicePort/processContainerModifyBusinessEvent/TestProcessContainerModifyBusinessEvent_guarAccomm_tpWithMultipleAccommTps.java
+++ src/test/java/com/disney/composite/api/accommodationModule/soapServices/accommodationSalesComponentServicePort/processContainerModifyBusinessEvent/TestProcessContainerModifyBusinessEvent_guarAccomm_tpWithMultipleAccommTps.java
@@ -45,12 +45,10 @@ public class TestProcessContainerModifyBusinessEvent_guarAccomm_tpWithMultipleAc
         tpsNum2 = getBook().getTravelPlanSegmentId();
         tcgNum2 = getBook().getTravelComponentGroupingId();
         TestReporter.logAPI(!getBook().getResponseStatusCode().equals("200"), "Verify that no error occurred booking a second TPS: " + getBook().getFaultString(), getBook());
-
     }
 
     @Test(groups = { "api", "regression", "accommodation", "accommodationSalesComponentService", "processContainerModifyBusinessEvent" })
     public void testProcessContainerModifyBusinessEvent_guarAccomm_tpWithMultipleAccommTps() {
-
         String tp = getBook().getTravelPlanId();
 
         AutoCancel ac = new AutoCancel(Environment.getBaseEnvironmentName(environment));
