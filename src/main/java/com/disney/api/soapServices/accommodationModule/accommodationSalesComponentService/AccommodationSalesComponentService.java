44d43d7270ecc37467486d701f6c060658620cf3
phlej001 <justin.x.phlegar.-nd@disney.com>
2018-02-13 16:22:13 -0500

    Adding temp fix for TDM redirect

--- src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesComponentService/AccommodationSalesComponentService.java
+++ src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesComponentService/AccommodationSalesComponentService.java
@@ -6,7 +6,7 @@ import com.disney.utils.Environment;
 public class AccommodationSalesComponentService extends BaseSoapService {
     public AccommodationSalesComponentService(String environment) {
         setEnvironmentServiceURL("AccommodationSalesComponentServicePort", environment);
-        if (environment.equalsIgnoreCase(Environment.getEnvironmentName("Bashful"))) {
+        if (environment.equalsIgnoreCase(Environment.getEnvironmentName("Bashful")) || environment.equalsIgnoreCase(Environment.getEnvironmentName("Latest"))) {
             setServiceURL("http://accommodationsvcbash.wdw.disney.com:8080/Accommodation/AccommodationSalesComponentServicePort");
         }
         setXmlRepo("xml/accommodationSalesComponentServicePort");
--- src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesServicePort/AccommodationSalesServicePort.java
+++ src/main/java/com/disney/api/soapServices/accommodationModule/accommodationSalesServicePort/AccommodationSalesServicePort.java
@@ -7,7 +7,7 @@ public class AccommodationSalesServicePort extends BaseSoapService {
     public AccommodationSalesServicePort(String environment) {
         setEnvironmentServiceURL("AccommodationSalesServicePort", environment);
 
-        if (environment.equalsIgnoreCase(Environment.getEnvironmentName("Bashful"))) {
+        if (environment.equalsIgnoreCase(Environment.getEnvironmentName("Bashful")) || environment.equalsIgnoreCase(Environment.getEnvironmentName("Latest"))) {
             setServiceURL("http://accommodationsvcbash.wdw.disney.com:8080/Accommodation/AccommodationSalesServicePort");
         }
 
