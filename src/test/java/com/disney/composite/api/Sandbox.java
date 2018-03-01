ef00e3aef6579c1e6afca98720421e23860b6861
averw003 <waightstill.w.avery.-nd@disney.com>
2018-03-01 09:43:35 -0500

    debug

--- src/test/java/com/disney/composite/api/Sandbox.java
+++ src/test/java/com/disney/composite/api/Sandbox.java
@@ -1,17 +1,39 @@
 package com.disney.composite.api;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.disney.api.soapServices.admissionModule.admissionSalesServicePort.helpers.pricingHelpers.RoomTypes;
-import com.disney.api.soapServices.pricingModule.packagingService.operations.helpers.PackageCodeHelper;
+import com.disney.api.soapServices.accommodationModule.accommodationSalesServicePort.operations.Retrieve;
+import com.disney.utils.TestReporter;
+import com.disney.utils.dataFactory.database.Database;
+import com.disney.utils.dataFactory.database.Recordset;
+import com.disney.utils.dataFactory.database.databaseImpl.OracleDatabase;
+import com.disney.utils.dataFactory.database.sqlStorage.Dreams_AccommodationQueries;
 
 public class Sandbox {
+    private String environment = "Latest";
+    private String tpsId;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setup() {
+        Database db = new OracleDatabase(environment, Database.DVC_POINTS);
+        Recordset rs = new Recordset(
+                db.getResultSet(
+                        Dreams_AccommodationQueries
+                                .getReservationInfoByTpId("480047528288")));
+        TestReporter.assertTrue(rs.getRowCount() > 0,
+                "Verify that folio charges were found form folio transaction ID [" + 82451375 + "].");
+        tpsId = rs.getValue("TPS_ID");
+    }
 
     @Test
     public void roomOnlyBooking() {
-        PackageCodeHelper helper = new PackageCodeHelper("latest", "2018-02-12", RoomTypes.getRoomOnly(), "DRC RO", "SWN", "SA", "2018-02-05");
-        System.out.println(helper.getPackageCode());
+        Retrieve retrieve = new Retrieve(environment, "ByTPS_ID");
+        retrieve.setTravelPlanSegmentId(tpsId);
+        retrieve.setLocationId("51");
+        retrieve.sendRequest();
+        TestReporter.logAPI(!retrieve.getResponseStatusCode().equals("200"),
+                "Verify that no error occurred retrieving: " + retrieve.getFaultString(),
+                retrieve);
     }
-
 }
-//
